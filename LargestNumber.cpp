17847c5fbc6637c274503d21ebce55b9d1f1ac03
Abhishek Agrawal <cruxrebels@gmail.com>
2016-04-20 00:33:02 +0530

    Optimized for large inputs. Foregone TLE error.
    auto tempx = x;
        auto tempy = y;
        auto cx = 0;
        auto cy = 0;
        while(tempx!=0 || tempy!=0)
        {
            if(tempx!=0)
            {
                tempx /= 10;
                ++cx;
            }
            if(tempy!=0)
            {
                tempy /= 10;
                ++cy;
            }
        }
        auto xy = (x*(pow(10, cy)))+y;
        auto yx = (y*(pow(10, cx)))+x;
        
    Was getting TLE for above solution except all the initialization was "int" instead of "auto".So, auto xy = (x*(pow(10, cy)))+y; FOR VERY LARGE x,y overflowed lie x = 12345678, y = 123456.
    
    Substituting with auto took care of widening the initialization accordingly and TLE was succeeded.

--- LargestNumber.cpp
+++ LargestNumber.cpp
@@ -1,9 +1,9 @@
 bool myCompare(const int x, const int y)
 {
-    int tempx = x;
-    int tempy = y;
-    int cx = 0;
-    int cy = 0;
+    auto tempx = x;
+    auto tempy = y;
+    auto cx = 0;
+    auto cy = 0;
     while(tempx!=0 || tempy!=0)
     {
         if(tempx!=0)
@@ -17,8 +17,8 @@ bool myCompare(const int x, const int y)
             ++cy;
         }
     }
-    int xy = (x*(pow(10, cy)))+y;
-    int yx = (y*(pow(10, cx)))+x;
+    auto xy = (x*(pow(10, cy)))+y;
+    auto yx = (y*(pow(10, cx)))+x;
     //cout << xy << "\t" << yx << "\t" << x << "\t" << y << "\n";
     return (xy>yx);
 }
@@ -39,10 +39,10 @@ string Solution::largestNumber(const vector<int> &A) {
         
     sort(B.begin(), B.end(), myCompare);
     
-    string result = "";
+    stringstream result;
     for(const auto& b : B)
-        result+=to_string(b);
+        result << b;
         
     //copy(B.begin(), B.end(), ostream_iterator<int>(result, ""));
-    return result;
+    return result.str();
 }
