07ae07ea1bced94d41912263fe62df20f18cf48a
Abhishek Agrawal <cruxrebels@gmail.com>
2016-04-20 02:10:23 +0530

    String approach for LargestNumber (More optimal)
    Best optimized solution, though previous commit was also valid. But this one is the best way!

--- LargestNumber.cpp
+++ LargestNumber.cpp
@@ -1,48 +1,22 @@
-bool myCompare(const int x, const int y)
-{
-    auto tempx = x;
-    auto tempy = y;
-    auto cx = 0;
-    auto cy = 0;
-    while(tempx!=0 || tempy!=0)
-    {
-        if(tempx!=0)
-        {
-            tempx /= 10;
-            ++cx;
+class Solution {
+    public:
+        static bool compareNum(string a, string b) {
+            return a + b > b + a;
         }
-        if(tempy!=0)
-        {
-            tempy /= 10;
-            ++cy;
-        }
-    }
-    auto xy = (x*(pow(10, cy)))+y;
-    auto yx = (y*(pow(10, cx)))+x;
-    //cout << xy << "\t" << yx << "\t" << x << "\t" << y << "\n";
-    return (xy>yx);
-}
 
-string Solution::largestNumber(const vector<int> &A) {
-    vector<int> B;
-    int c = 0;
-    stringstream r;
-    r << 0;
-    for(auto const& a : A)
-    {
-        B.emplace_back(a);
-        if (a==0)
-            ++c;
-    }
-    if (c==A.size())
-        return r.str();
-        
-    sort(B.begin(), B.end(), myCompare);
-    
-    stringstream result;
-    for(const auto& b : B)
-        result << b;
-        
-    //copy(B.begin(), B.end(), ostream_iterator<int>(result, ""));
-    return result.str();
-}
+        string largestNumber(const vector<int> &num) {
+            string result;
+            vector<string> str;
+            for (int i = 0; i < num.size(); i++) {
+                str.push_back(to_string(num[i]));
+            }
+            sort(str.begin(), str.end(), compareNum);
+            for (int i = 0; i < str.size(); i++) {
+                result += str[i];
+            }
+
+            int pos = 0;
+            while (result[pos] == '0' && pos + 1 < result.size()) pos++; 
+            return result.substr(pos);
+        }
+};
