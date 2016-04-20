1ee438b5874cc2f1180654da1fe5020bf17eeea6
Abhishek Agrawal <cruxrebels@gmail.com>
2016-04-20 21:34:44 +0530

    Update FindMissingInteger.cpp
    Added the code.

--- FindMissingInteger.cpp
+++ FindMissingInteger.cpp
@@ -1,1 +1,17 @@
-
+int Solution::firstMissingPositive(vector<int> &A) {
+    sort(A.begin(), A.end());
+    int c = 1;
+    for (auto const& a : A)
+    {
+        if (a<1)
+            continue;
+        else if (a==c)
+        {
+            ++c;
+            continue;
+        }
+        else
+            break;
+    }
+    return c;
+}
