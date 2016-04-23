96949e1c7a255db085b1becdc5b00971e993a784
Abhishek Agrawal <cruxrebels@gmail.com>
2016-04-24 00:58:08 +0530

    Update AllFactors.cpp
    Added the code.

--- AllFactors.cpp
+++ AllFactors.cpp
@@ -1,1 +1,17 @@
-
+vector<int> Solution::allFactors(int A) {
+    vector<int> result;
+ 
+    for(auto i=1; i<=sqrt(A); ++i)
+    {
+        if(A%i==0)
+        {
+            result.emplace_back(i);
+            if(i!=sqrt(A))
+            {
+                result.emplace_back(A/i);
+            }
+        }
+    }
+    sort(result.begin(), result.end());
+    return result;
+}
