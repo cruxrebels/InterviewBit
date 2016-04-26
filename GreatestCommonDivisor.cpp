0957b1303ce7e5cfcf99d00d195286efa76d97c6
Abhishek Agrawal <cruxrebels@gmail.com>
2016-04-27 00:14:53 +0530

    Update GreatestCommonDivisor.cpp
    Added the code.

--- GreatestCommonDivisor.cpp
+++ GreatestCommonDivisor.cpp
@@ -1,1 +1,16 @@
+int Solution::gcd(int A, int B) {
+    //Euclid's algorithm
+    while(B!=0)
+    {
+        int r = A%B;
+        A = B;
+        B = r;
+    }
+    return A;
+}
 
+//Recursive one line solution
+/*int Solution::gcd(int A, int B) {
+    //Euclid's algorithm
+    return B==0 ? A : gcd(B, A%B);
+}*/
