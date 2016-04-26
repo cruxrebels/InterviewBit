11b28a1bd5deb29026c49b757cf4da54042c4cb2
Abhishek Agrawal <cruxrebels@gmail.com>
2016-04-26 21:24:37 +0530

    Update BinaryRepresentation.cpp
    Added the code.

--- BinaryRepresentation.cpp
+++ BinaryRepresentation.cpp
@@ -1,1 +1,16 @@
+string Solution::findDigitsInBinary(int A) {
+    int r;
+    string result;
+    
+    if(A==0)
+        return "0"; //prev used result += to_string(0); return result;
 
+    while(A>0)
+    {
+        r = A%2;
+        result += to_string(r); //Or result.push_back((char)('0' + r)); will also work
+        A /= 2;
+    }
+    reverse(result.begin(), result.end());
+    return result;
+}
