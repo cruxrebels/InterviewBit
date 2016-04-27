79b596c0cf63442810606729ae87afa91ac4ac9f
Abhishek Agrawal <cruxrebels@gmail.com>
2016-04-28 00:43:06 +0530

    Update PowerOfTwoIntegers.cpp
    Added the code.

--- PowerOfTwoIntegers.cpp
+++ PowerOfTwoIntegers.cpp
@@ -1,1 +1,14 @@
-
+bool Solution::isPower(int A) {
+    if (A<2)
+        return true;
+  
+    for (auto i = 2; i<=sqrt(A); ++i)
+    {
+        for (auto j = 2; j<=32; ++j)
+        {
+            if(pow(i, j)==A)
+                return true;
+        }
+    }
+    return false;
+}
