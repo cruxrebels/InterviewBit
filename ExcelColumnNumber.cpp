bc68ac06124853f443bb4493b73a303cd19cb898
Abhishek Agrawal <cruxrebels@gmail.com>
2016-04-29 02:27:50 +0530

    Update ExcelColumnNumber.cpp
    Added the code.

--- ExcelColumnNumber.cpp
+++ ExcelColumnNumber.cpp
@@ -1,1 +1,9 @@
-
+int Solution::titleToNumber(string A) {
+     auto n = A.length();
+     int value = 0;
+     for (auto i=0; i<n; ++i)
+     {
+        value += pow(26, i)*(A[n-(i+1)] - 'A' + 1);
+     }
+     return value;
+}
