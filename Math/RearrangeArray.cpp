888d1038224a60aa10cad125370703987c76a737
Abhishek Agrawal <cruxrebels@gmail.com>
2016-04-30 20:35:07 +0530

    Create RearrangeArray.cpp
    Added the question.

---
+++ Math/RearrangeArray.cpp
@@ -0,0 +1,11 @@
+/*Rearrange a given array so that Arr[i] becomes Arr[Arr[i]] with O(1) extra space.
+
+Example:
+
+Input : [1, 0]
+Return : [0, 1]
+ Lets say N = size of the array. Then, following holds true :
+* All elements in the array are in the range [0, N-1]
+* N * N does not overflow for a signed integer 
+
+https://www.interviewbit.com/problems/rearrange-array/ */
