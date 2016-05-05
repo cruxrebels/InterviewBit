d558d0fe386f1bc319192002d1d28e8f1c5f6ccd
Abhishek Agrawal <cruxrebels@gmail.com>
2016-05-06 00:23:40 +0530

    Update SortedInsertPosition.cpp
    Added the code.

--- BinarySearch/SortedInsertPosition.cpp
+++ BinarySearch/SortedInsertPosition.cpp
@@ -1,1 +1,35 @@
+/*
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
+You may assume no duplicates in the array.
+
+Here are few examples.
+
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0
+
+https://www.interviewbit.com/problems/sorted-insert-position/
+*/
+
+int Solution::searchInsert(vector<int> &A, int B) {
+    if(B<1)
+        return 0;
+    int start = 0, end = A.size() - 1;
+    int pos = -1;
+    
+    while(start<=end)
+    {
+        int mid = start + (end - start)/2;
+        if(A[mid] == B)
+            return mid;
+        else if(A[mid]>B)
+            pos = end = mid - 1;
+        else
+            pos = start = mid + 1;
+        if (start>end)
+            pos = start;
+    }
+    return pos;
+}
