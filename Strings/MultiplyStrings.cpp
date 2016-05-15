f7d8009cabb2b34395e54aab9b6b10d31cdad519
Abhishek Agrawal <cruxrebels@gmail.com>
2016-05-16 01:04:37 +0530

    Update MultiplyStrings.cpp
    Added the problem statement.

--- Strings/MultiplyStrings.cpp
+++ Strings/MultiplyStrings.cpp
@@ -1,1 +1,14 @@
+/*
+Given two numbers represented as strings, return multiplication of the numbers as a string.
+
+ Note: The numbers can be arbitrarily large and are non-negative.
+Note2: Your answer should not have leading zeroes. For example, 00 is not a valid answer. 
+For example, 
+given strings "12", "10", your answer should be “120”.
+
+NOTE : DO NOT USE BIG INTEGER LIBRARIES ( WHICH ARE AVAILABLE IN JAVA / PYTHON ). 
+We will retroactively disqualify such submissions and the submissions will incur penalties.
+
+https://www.interviewbit.com/problems/multiply-strings/
+*/
 
