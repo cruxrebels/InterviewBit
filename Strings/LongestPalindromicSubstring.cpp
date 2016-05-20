139c59f374033ec9a525f6d517c0196a004c6519
Abhishek Agrawal <cruxrebels@gmail.com>
2016-05-20 22:22:22 +0530

    Update LongestPalindromicSubstring.cpp
    Added the code.

--- Strings/LongestPalindromicSubstring.cpp
+++ Strings/LongestPalindromicSubstring.cpp
@@ -1,1 +1,46 @@
+/*
+Given a string S, find the longest palindromic substring in S.
 
+Substring of string S:
+
+S[i...j] where 0 <= i <= j < len(S)
+
+Palindrome string:
+
+A string which reads the same backwards. More formally, S is palindrome if reverse(S) = S.
+
+Incase of conflict, return the substring which occurs first ( with the least starting index ).
+
+Example :
+
+Input : "aaaabaaa"
+Output : "aaabaaa"
+
+https://www.interviewbit.com/problems/longest-palindromic-substring/
+*/
+
+string expandAroundCenter(string s, int c1, int c2) {
+  int l = c1, r = c2;
+  int n = s.length();
+  while (l >= 0 && r <= n-1 && s[l] == s[r]) {
+    l--;
+    r++;
+  }
+  return s.substr(l+1, r-l-1);
+}
+ 
+string Solution::longestPalindrome(string s) {
+  int n = s.length();
+  if (n == 0) return "";
+  string longest = s.substr(0, 1);  // a single char itself is a palindrome
+  for (int i = 0; i < n-1; i++) {
+    string p1 = expandAroundCenter(s, i, i);
+    if (p1.length() > longest.length())
+      longest = p1;
+ 
+    string p2 = expandAroundCenter(s, i, i+1);
+    if (p2.length() > longest.length())
+      longest = p2;
+  }
+  return longest;
+}
