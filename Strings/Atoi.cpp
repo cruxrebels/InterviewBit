029703a122a96dc5b85ec026e1420716f0ff2d9e
Abhishek Agrawal <cruxrebels@gmail.com>
2016-05-22 23:49:37 +0530

    Update Atoi.cpp
    Added the problem statement.

--- Strings/Atoi.cpp
+++ Strings/Atoi.cpp
@@ -1,1 +1,19 @@
+/*
+Implement atoi to convert a string to an integer.
 
+Example :
+
+Input : "9 2704"
+Output : 9
+Note: There might be multiple corner cases here. Clarify all your doubts using “See Expected Output”.
+
+ Questions: Q1. Does string contain whitespace characters before the number?
+A. Yes Q2. Can the string have garbage characters after the number?
+A. Yes. Ignore it. Q3. If no numeric character is found before encountering garbage characters, what should I do?
+A. Return 0. Q4. What if the integer overflows?
+A. Return INT_MAX if the number is positive, INT_MIN otherwise. 
+Warning : DO NOT USE LIBRARY FUNCTION FOR ATOI.
+If you do, we will disqualify your submission retroactively and give you penalty points.
+
+https://www.interviewbit.com/problems/atoi/
+*/
