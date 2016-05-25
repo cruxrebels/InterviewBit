80a64a88e8df0a9d12a505b2debcd839e8022858
Abhishek Agrawal <cruxrebels@gmail.com>
2016-05-26 00:07:15 +0530

    Update SingleNumber.cpp
    Added the code.

--- BitManipulation/SingleNumber.cpp
+++ BitManipulation/SingleNumber.cpp
@@ -12,3 +12,11 @@ See Expected Output
 https://www.interviewbit.com/problems/single-number/
 */
 
+int Solution::singleNumber(const vector<int> &A) {
+    /*int temp = 0;                 //standard approach
+    for (auto i=0; i<A.size(); ++i)
+        temp ^= A[i];
+    return temp;*/
+    
+    return accumulate(A.begin(), A.end(), 0, bit_xor<int>()); //std library method
+}
