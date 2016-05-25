53c36736ef05bea7b2a92751ad4ee8b63c274a3a
Abhishek Agrawal <cruxrebels@gmail.com>
2016-05-26 00:06:49 +0530

    Create SingleNumber.cpp
    Creating the file.

---
+++ BitManipulation/SingleNumber.cpp
@@ -0,0 +1,14 @@
+/*
+Given an array of integers, every element appears twice except for one. Find that single one.
+
+Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example :
+
+Input : [1 2 2 3 1]
+Output : 3
+See Expected Output
+
+https://www.interviewbit.com/problems/single-number/
+*/
+
