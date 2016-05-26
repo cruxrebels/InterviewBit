797150d7cc19081488b9205f4798bc8a95916832
Abhishek Agrawal <cruxrebels@gmail.com>
2016-05-27 02:22:11 +0530

    Update SingleNumberII.cpp
    Added the code.

--- BitManipulation/SingleNumberII.cpp
+++ BitManipulation/SingleNumberII.cpp
@@ -1,1 +1,45 @@
+/*
+Given an array of integers, every element appears thrice except for one which occurs once.
 
+Find that element which does not appear thrice.
+
+Note: Your algorithm should have a linear runtime complexity.
+
+Could you implement it without using extra memory?
+
+Example :
+
+Input : [1, 2, 4, 3, 3, 2, 2, 3, 1, 1]
+Output : 4
+
+https://www.interviewbit.com/problems/single-number-ii/
+*/
+
+int Solution::singleNumber(const vector<int> &A) {
+    int result = 0;
+    for (auto i=0; i<32; ++i) //works for any occurence of number but only on 32 bit system
+    {
+        int sum = 0;
+        for (auto j=0; j<A.size(); ++j)
+        {
+            if ((A[j] >> i) & 1 == 1)
+            {
+                ++sum;
+                sum %= 3;
+            }
+        }
+        if (sum!=0)
+            result |= sum << i;
+    }
+    return result;
+    /*int t1 = 0, t2 = 0, t3 = 0;
+    for (int i = 0; i < A.size(); ++i)
+    {
+        t1 ^= A[i];
+        t2 = t2 | ((t1^A[i]) & A[i]);
+        t3 = ~(t1 & t2);
+        t1 = t1 & t3;
+        t2 = t2 & t3;
+    }
+    return t1;*/
+}
