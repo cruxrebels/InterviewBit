7702756161a1a2434dca10ead54ad8b25ff1dc88
Abhishek Agrawal <cruxrebels@gmail.com>
2016-05-28 00:00:04 +0530

    Create ReverseBits.cpp
    Creating the file with problem statement.

---
+++ BitManipulation/ReverseBits.cpp
@@ -0,0 +1,21 @@
+/*
+Reverse bits of an 32 bit unsigned integer
+
+Example 1:
+
+x = 0,
+
+          00000000000000000000000000000000  
+=>        00000000000000000000000000000000
+return 0
+
+Example 2:
+
+x = 3,
+
+          00000000000000000000000000000011 
+=>        11000000000000000000000000000000
+return 3221225472
+
+https://www.interviewbit.com/problems/reverse-bits/
+*/
