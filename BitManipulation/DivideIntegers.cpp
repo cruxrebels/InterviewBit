0ab9324e77dd159044f750b0439fc955214e51c7
Abhishek Agrawal <cruxrebels@gmail.com>
2016-05-29 01:52:27 +0530

    Update DivideIntegers.cpp
    Added the code.

--- BitManipulation/DivideIntegers.cpp
+++ BitManipulation/DivideIntegers.cpp
@@ -1,1 +1,39 @@
+/*
+Divide two integers without using multiplication, division and mod operator.
 
+Return the floor of the result of the division.
+
+Example:
+
+5 / 2 = 2
+Also, consider if there can be overflow cases. For overflow case, return INT_MAX.
+
+https://www.interviewbit.com/problems/divide-integers/
+*/
+
+int Solution::divide(int dividend, int divisor) {
+    int sign = 1;
+    if (dividend<0){sign = -sign;}
+    if (divisor<0){sign = -sign;}
+     
+    unsigned long long tmp = abs((long long)dividend);
+    unsigned long long tmp2 = abs((long long)divisor);
+            
+    unsigned long c = 1;
+    while (tmp2 < tmp){
+        tmp2 = tmp2<<1;    
+        c = c<<1;
+    }
+     
+    long long res = 0;
+    while (tmp>=abs((long long)divisor)){
+        while (tmp2 <= tmp){
+            tmp -= tmp2;
+            res = res+c;
+        }
+        tmp2=tmp2>>1;
+        c=c>>1;    
+    }
+     
+    return (sign*res >= INT_MAX ||  sign*res < INT_MIN) ? INT_MAX : sign*res;
+}
