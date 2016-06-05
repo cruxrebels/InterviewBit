a78d6ff6d8fc4ad63149a958d143c1a00f85d0da
Abhishek Agrawal <cruxrebels@gmail.com>
2016-06-06 00:16:45 +0530

    Update ContainerWithMostWater.cpp
    Added the problem statement.

--- TwoPointers/ContainerWithMostWater.cpp
+++ TwoPointers/ContainerWithMostWater.cpp
@@ -1,1 +1,21 @@
+/*
+Given n non-negative integers a1, a2, ..., an,
+where each represents a point at coordinate (i, ai).
+'n' vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+
+Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+Your program should return an integer which corresponds to the maximum area of water that can be contained ( Yes, we know maximum area instead of maximum volume sounds weird. But this is 2D plane we are working with for simplicity ).
+
+ Note: You may not slant the container. 
+Example :
+
+Input : [1, 5, 4, 3]
+Output : 6
+
+Explanation : 5 and 3 are distance 2 apart. So size of the base = 2. Height of container = min(5, 3) = 3. 
+So total area = 3 * 2 = 6
+
+https://www.interviewbit.com/problems/container-with-most-water/
+*/
 
