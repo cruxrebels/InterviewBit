c50a17b0a12a89140494eaba189cf0dd1010c620
Abhishek Agrawal <cruxrebels@gmail.com>
2016-06-13 23:52:15 +0530

    Update RemoveDuplicatesFromSortedList.cpp
    Added the code.

--- RemoveDuplicatesFromSortedList.cpp
+++ RemoveDuplicatesFromSortedList.cpp
@@ -1,1 +1,10 @@
+/*
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+For example,
+Given 1->1->2, return 1->2.
+Given 1->1->2->3->3, return 1->2->3.
+
+https://www.interviewbit.com/problems/remove-duplicates-from-sorted-list/
+*/
 
