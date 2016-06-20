7e22cee4de178a65cdf306b2455ab89d537a3fae
Abhishek Agrawal <cruxrebels@gmail.com>
2016-06-21 03:22:03 +0530

    Update InsertionSortList.cpp
    Added the problem statement.

--- LinkedLists/InsertionSortList.cpp
+++ LinkedLists/InsertionSortList.cpp
@@ -1,1 +1,16 @@
+/*
+Sort a linked list using insertion sort.
+
+We have explained Insertion Sort at Slide 7 of Arrays
+
+Insertion Sort Wiki has some details on Insertion Sort as well.
+
+Example :
+
+Input : 1 -> 3 -> 2
+
+Return 1 -> 2 -> 3
+
+https://www.interviewbit.com/problems/insertion-sort-list/
+*/
 
