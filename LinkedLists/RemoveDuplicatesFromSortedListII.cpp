e5c00068a724403706041a7a98823a6a73cccb8f
Abhishek Agrawal <cruxrebels@gmail.com>
2016-07-02 01:11:37 +0530

    Update RemoveDuplicatesFromSortedListII.cpp
    Added the code.

--- LinkedLists/RemoveDuplicatesFromSortedListII.cpp
+++ LinkedLists/RemoveDuplicatesFromSortedListII.cpp
@@ -8,3 +8,32 @@ Given 1->1->1->2->3, return 2->3.
 https://www.interviewbit.com/problems/remove-duplicates-from-sorted-list-ii/
 */
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::deleteDuplicates(ListNode* A) {
+    if(!A || !A->next)
+        return A;
+        
+    ListNode* fakeHead = new ListNode(0);
+    fakeHead->next = A;
+    ListNode* prev = fakeHead;
+    while (A)
+    {
+        while (A->next && A->val == A->next->val)
+            A = A->next;
+        
+        if (prev->next == A)
+            prev = prev->next; 
+        else 
+            prev->next = A->next;
+            
+        A = A->next;
+    }
+    return fakeHead->next;
+}
