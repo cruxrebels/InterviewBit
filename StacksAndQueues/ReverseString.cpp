ea332e9e459badbd6510343ba62ff4589c66e218
Abhishek Agrawal <cruxrebels@gmail.com>
2016-07-03 23:33:23 +0530

    Update ReverseString.cpp
    Added better space complexity code.

--- StacksAndQueues/ReverseString.cpp
+++ StacksAndQueues/ReverseString.cpp
@@ -11,19 +11,20 @@ https://www.interviewbit.com/problems/reverse-string/
 
 string Solution::reverseString(string A) {
     stack<char> s;
-    string result = "";
     auto size = A.length();
     
     for(auto i=0; i<size; ++i)
         s.push(A[i]);
-    
+    A.clear();
     for(auto j=0; j<size; ++j)
     {
         if(!s.empty())
         {
-            result += s.top();
+            A += s.top();
             s.pop();
         }
     }
+    return A;
+}
     return result;
 }
