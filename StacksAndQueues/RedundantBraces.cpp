ad3e2ebfff04ace30ab1d195caaabb88cdc73858
Abhishek Agrawal <cruxrebels@gmail.com>
2016-07-07 21:38:48 +0530

    Update RedundantBraces.cpp
    Added the code.

--- StacksAndQueues/RedundantBraces.cpp
+++ StacksAndQueues/RedundantBraces.cpp
@@ -7,6 +7,35 @@ Input will be always a valid expression
 
 and operators allowed are only + , * , - , /
 
+Example:
+
+((a+b)) has redundant braces so answer will be 1
+(a + (a + b)) doesn't have have any redundant braces so answer will be 0
+
 https://www.interviewbit.com/problems/redundant-braces/
 */
 
+int Solution::braces(string A) {
+    stack<char> s;
+    auto size = A.length();
+    auto i = 0;
+    while(i<size)
+    {
+        char c = A[i];
+        if (c == '(' || c == '+' || c == '*' || c == '-' || c == '/')
+            s.push(c);
+        else if (c == ')')
+        {
+            if (s.top() == '(')
+                return 1;
+            else
+            {
+                while (!s.empty() && s.top() != '(')
+                    s.pop();
+                s.pop();
+            }
+        }
+        ++i;
+    }
+    return 0;
+}
