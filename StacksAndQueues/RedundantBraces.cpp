f11b13d0144746ff3c59ede9ca516fca7495cc4c
Abhishek Agrawal <cruxrebels@gmail.com>
2016-07-07 21:37:55 +0530

    Update RedundantBraces.cpp
    Added the problem statement.

--- StacksAndQueues/RedundantBraces.cpp
+++ StacksAndQueues/RedundantBraces.cpp
@@ -1,1 +1,12 @@
+/*
+Write a program to validate if the input string has redundant braces?
+Return 0/1 
+ 0 -> NO 1 -> YES 
+
+Input will be always a valid expression
+
+and operators allowed are only + , * , - , /
+
+https://www.interviewbit.com/problems/redundant-braces/
+*/
 
