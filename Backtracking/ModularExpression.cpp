60af844eea16e1fe5ca33ab87d1d7e43db3f70de
Abhishek Agrawal <cruxrebels@gmail.com>
2016-07-12 01:27:37 +0530

    Update ModularExpression.cpp
    Added the code.

--- Backtracking/ModularExpression.cpp
+++ Backtracking/ModularExpression.cpp
@@ -1,1 +1,26 @@
+/*
+Implement pow(A, B) % C.
 
+In other words, given A, B and C, 
+find (AB)%C.
+
+Input : A = 2, B = 3, C = 3
+Return : 2 
+2^3 % 3 = 8 % 3 = 2
+
+https://www.interviewbit.com/problems/modular-expression/
+*/
+
+int Solution::Mod(int A, int B, int C) {
+    if (B == 0) return 1 % C;
+    long long y = 0;        
+    if (B % 2 == 0) {
+        y = Mod(A, B/2, C);
+        y = (y * y) % C;
+    } else {
+        y = A % C;
+        y = (y * Mod(A, B - 1, C)) % C;
+    }
+    // Incase y is negative. 
+    return (int)((y + C) % C);
+}
