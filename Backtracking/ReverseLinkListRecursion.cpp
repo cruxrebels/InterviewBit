a126c902f43573a7f0d07637d2362defb0ed73e5
Abhishek Agrawal <cruxrebels@gmail.com>
2016-07-12 01:14:06 +0530

    Update ReverseLinkListRecursion.cpp
    Added the code.

--- Backtracking/ReverseLinkListRecursion.cpp
+++ Backtracking/ReverseLinkListRecursion.cpp
@@ -1,1 +1,29 @@
+/*
+Reverse a linked list using recursion.
 
+Example :
+Given 1->2->3->4->5->NULL,
+
+return 5->4->3->2->1->NULL.
+
+https://www.interviewbit.com/problems/reverse-link-list-recursion/
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::reverseList(ListNode* A) {
+    if (!A || !A->next)
+        return A;
+    
+    ListNode* head = reverseList(A->next);
+    A->next->next = A;
+    A->next = NULL;
+    
+    return head;
+}
