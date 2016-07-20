a4f57555ec1b69dc7afb3bcc0d5c096d98ca5161
Abhishek Agrawal <cruxrebels@gmail.com>
2016-07-21 02:03:14 +0530

    Update CombinationSum.cpp
    Added the code.

--- Backtracking/CombinationSum.cpp
+++ Backtracking/CombinationSum.cpp
@@ -1,1 +1,57 @@
+/*
+Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
 
+The same repeated number may be chosen from C unlimited number of times.
+
+ Note:
+All numbers (including target) will be positive integers.
+Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+The combinations themselves must be sorted in ascending order.
+CombinationA > CombinationB iff (a1 > b1) OR (a1 = b1 AND a2 > b2) OR … (a1 = b1 AND a2 = b2 AND … ai = bi AND ai+1 > bi+1)
+The solution set must not contain duplicate combinations.
+Example, 
+Given candidate set 2,3,6,7 and target 7,
+A solution set is:
+
+[2, 2, 3]
+[7]
+ Warning : DO NOT USE LIBRARY FUNCTION FOR GENERATING COMBINATIONS.
+Example : itertools.combinations in python.
+If you do, we will disqualify your submission retroactively and give you penalty points. 
+See Expected Output
+
+https://www.interviewbit.com/problems/combination-sum/
+*/
+
+void backtracking(int start, vector<int>& row, int sum, vector<vector<int> >& res, vector<int>& A, int B)
+{
+    if (sum >= B)
+    {
+        if (sum==B)
+            res.emplace_back(row);
+        return;
+    }
+    
+    if (start == A.size())
+        return;
+        
+    row.emplace_back(A[start]);
+    sum += A[start];
+    backtracking(start, row, sum, res, A, B);
+    sum -= row[row.size()-1];
+    row.pop_back();
+    backtracking(start+1, row, sum, res, A, B);
+}
+vector<vector<int> > Solution::combinationSum(vector<int> &A, int B) {
+    vector<vector<int> > res;
+    vector<int> row, a;
+    sort(A.begin(), A.end());
+    
+    a.emplace_back(A[0]);
+    for (auto i = 1; i<A.size(); ++i)
+        if (A[i-1] != A[i])
+            a.emplace_back(A[i]);
+    
+    backtracking(0, row, 0, res, a, B);
+    return res;
+}
