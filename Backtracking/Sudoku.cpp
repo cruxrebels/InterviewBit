7b05a972fef538f4dcd9f7a0c84ea509198f1117
Abhishek Agrawal <cruxrebels@gmail.com>
2016-07-23 01:17:04 +0530

    Create Sudoku.cpp
    Creating the file.

---
+++ Backtracking/Sudoku.cpp
@@ -0,0 +1,16 @@
+/*
+Write a program to solve a Sudoku puzzle by filling the empty cells.
+Empty cells are indicated by the character '.' 
+You may assume that there will be only one unique solution.
+
+Example :
+
+For the above given diagrams, the corresponding input to your program will be
+
+[[53..7....], [6..195...], [.98....6.], [8...6...3], [4..8.3..1], [7...2...6], [.6....28.], [...419..5], [....8..79]]
+and we would expect your program to modify the above array of array of characters to
+
+[[534678912], [672195348], [198342567], [859761423], [426853791], [713924856], [961537284], [287419635], [345286179]]
+See Expected Output
+NotesAll Notes
+*/
