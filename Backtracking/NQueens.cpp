5d2aa31470439a53d404a0f2b3bf3f232ead1c8f
Abhishek Agrawal <cruxrebels@gmail.com>
2016-07-23 01:14:13 +0530

    Update NQueens.cpp
    Removed horizontal scroll bar need inside code window.

--- Backtracking/NQueens.cpp
+++ Backtracking/NQueens.cpp
@@ -3,7 +3,8 @@ The n-queens puzzle is the problem of placing n queens on an n×n chessboard suc
 
 Given an integer n, return all distinct solutions to the n-queens puzzle.
 
-Each solution contains a distinct board configuration of the n-queens’ placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+Each solution contains a distinct board configuration of the n-queens’ placement, where 'Q' and '.' both indicate a queen 
+and an empty space respectively.
 
 For example,
 There exist two distinct solutions to the 4-queens puzzle:
