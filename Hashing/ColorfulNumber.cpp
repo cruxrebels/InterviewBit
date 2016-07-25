f12f9c5a71ce4f3085f9d9a2d145d31bd7b13d91
Abhishek Agrawal <cruxrebels@gmail.com>
2016-07-25 21:33:50 +0530

    Update ColorfulNumber.cpp
    Added the problem statement.

--- Hashing/ColorfulNumber.cpp
+++ Hashing/ColorfulNumber.cpp
@@ -1,1 +1,23 @@
+/*
+For Given Number N find if its COLORFUL number or not
 
+Return 0/1
+
+COLORFUL number:
+
+A number can be broken into different contiguous sub-subsequence parts. 
+Suppose, a number 3245 can be broken into parts like 3 2 4 5 32 24 45 324 245. 
+And this number is a COLORFUL number, since product of every digit of a contiguous subsequence is different
+Example:
+
+N = 23
+2 3 23
+2 -> 2
+3 -> 3
+23 -> 6
+this number is a COLORFUL number since product of every digit of a sub-sequence are different. 
+
+Output : 1
+
+https://www.interviewbit.com/problems/colorful-number/
+*/
