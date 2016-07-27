e718157c3c8d4b3e0e36967522b4468f32ac744d
Abhishek Agrawal <cruxrebels@gmail.com>
2016-07-28 00:04:20 +0530

    2Sum.cpp
    Creating the file.

---
+++ Hashing/2Sum.cpp
@@ -0,0 +1,13 @@
+/*
+Given an array of integers, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 < index2. Please note that your returned answers (both index1 and index2 ) are not zero-based. 
+Put both these numbers in order in an array and return the array from your function ( Looking at the function signature will make things clearer ). Note that, if no pair exists, return empty list.
+
+If multiple solutions exist, output the one where index2 is minimum. If there are multiple solutions with the minimum index2, choose the one with minimum index1 out of them.
+
+Input: [2, 7, 11, 15], target=9
+Output: index1 = 1, index2 = 2
+
+https://www.interviewbit.com/problems/2-sum/
+*/
