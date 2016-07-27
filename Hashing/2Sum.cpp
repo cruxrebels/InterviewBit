b77c88bf582cfa5709f0cff3160d412e56e820de
Abhishek Agrawal <cruxrebels@gmail.com>
2016-07-28 00:04:49 +0530

    Update 2Sum.cpp
    Added the code.

--- Hashing/2Sum.cpp
+++ Hashing/2Sum.cpp
@@ -11,3 +11,23 @@ Output: index1 = 1, index2 = 2
 
 https://www.interviewbit.com/problems/2-sum/
 */
+
+vector<int> Solution::twoSum(const vector<int> &A, int B) {
+    vector<int> res;
+    auto size = A.size();
+    unordered_map<int, int> hash;
+    
+    for (auto i = 0; i<size; ++i)
+    {
+        if (hash.find(B - A[i]) != hash.end())
+        {
+            res.emplace_back(hash[B - A[i]]);
+            res.emplace_back(i+1);
+            return res;
+        }
+        if (hash.find(A[i]) == hash.end())
+            hash[A[i]] = i+1;
+    }
+    
+    return res;
+}
