7a55750c43bb6f2c2020b365ec0df9ab875d50e9
Abhishek Agrawal <cruxrebels@gmail.com>
2016-07-31 20:26:44 +0530

    Update 4Sum.cpp
    Added more detailed problem statement.

--- Hashing/4Sum.cpp
+++ Hashing/4Sum.cpp
@@ -1,7 +1,19 @@
 /*
-/*
- *Given an array S of n integers, are there elements a, b, c, and d in S such that
- *a + b + c + d = target ?
- *Find all unique quadruplets in the array which gives the sum of target.
- */
+Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+
+ Note:
+Elements in a quadruplet (a,b,c,d) must be in non-descending order. (ie, a ≤ b ≤ c ≤ d)
+The solution set must not contain duplicate quadruplets.
+Example : 
+Given array S = {1 0 -1 0 -2 2}, and target = 0
+A solution set is:
+
+    (-2, -1, 1, 2)
+    (-2,  0, 0, 2)
+    (-1,  0, 0, 1)
+Also make sure that the solution set is lexicographically sorted.
+Solution[i] < Solution[j] iff Solution[i][0] < Solution[j][0] OR (Solution[i][0] == Solution[j][0] AND ... Solution[i][k] < Solution[j][k])
+
+https://www.interviewbit.com/problems/4-sum/
+*/
 
