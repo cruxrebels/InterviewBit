966a2c304e4cc3d97c88343007a943dc235ba71f
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-01 01:03:30 +0530

    Update DiffkII.cpp
    Added the code.

--- Hashing/DiffkII.cpp
+++ Hashing/DiffkII.cpp
@@ -1,1 +1,38 @@
+/*
+Given an array A of integers and another non negative integer k, find if there exists 2 indices i and j such that A[i] - A[j] = k, i != j.
 
+Example :
+
+Input :
+
+A : [1 5 3]
+k : 2
+Output :
+
+1
+as 3 - 1 = 2
+
+Return 0 / 1 for this problem.
+See Expected Output
+
+https://www.interviewbit.com/problems/diffk-ii/
+*/
+
+int Solution::diffPossible(const vector<int> &A, int B) {
+    vector<int> input(A);
+    sort(input.begin(), input.end());
+    auto p = 0, q = 0;
+    auto size = input.size();
+    while (q < size)
+    {
+        if (p == q)
+            ++q;
+        else if (input[q]-input[p] == B && p!=q)
+            return 1;
+        else if (input[q]-input[p] < B)
+            ++q;
+        else if (input[q]-input[p] > B)
+            ++p;
+    }
+    return 0;
+}
