c12e4ddb876f64ba9ddd3b790acd9f6ff5a724e2
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-02 00:17:33 +0530

    Update Anagrams.cpp
    Added the code.

--- Hashing/Anagrams.cpp
+++ Hashing/Anagrams.cpp
@@ -16,4 +16,25 @@ The indices are 1 based ( the first element has index 1 instead of index 0).
  https://www.interviewbit.com/problems/anagrams/
  */
  
- 
+ vector<vector<int> > Solution::anagrams(const vector<string> &A) {
+    vector<vector<int> > res;
+    vector<string> row;
+    unordered_map<string, vector<int> > anagram;
+    auto size = A.size();
+    
+    for (auto i = 0; i<size; ++i)
+    {
+        string temp;
+        temp.append(A[i]);
+        sort(temp.begin(), temp.end());
+        row.emplace_back(temp);
+    }
+    
+    for (auto j = 0; j<size; ++j)
+        anagram[row[j]].emplace_back(j+1);
+    
+    for (auto it = anagram.begin(); it != anagram.end(); ++it)
+        res.emplace_back(it->second);
+    
+    return res;
+}
