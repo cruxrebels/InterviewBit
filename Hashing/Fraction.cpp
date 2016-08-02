056e286156bb6b1fa852ea464eb8937d1e54d71a
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-03 01:28:56 +0530

    Update Fraction.cpp
    Added the code.

--- Hashing/Fraction.cpp
+++ Hashing/Fraction.cpp
@@ -11,3 +11,68 @@ Given numerator = 2, denominator = 3, return "0.(6)"
 
 https://www.interviewbit.com/problems/fraction/
 */
+
+string Solution::fractionToDecimal(int numerator, int denominator) {
+    if (numerator == 0)
+        return "0";
+    int64_t n = numerator, d = denominator;
+    string res = "";
+    
+    if (n < 0 ^ d < 0)      //Brilliant
+        res += '-';
+    
+    n = abs(n), d = abs(d);
+    
+    res += to_string(n/d);
+    int64_t rem = n%d;
+    
+    if (rem == 0)
+        return res;
+    
+    res += '.';
+    
+    unordered_map<int, int> m;
+    
+    for (; rem; rem %= d)
+    {
+        if (m.find(rem) != m.end())
+        {
+            res.insert(m[rem], 1, '(');
+            res.push_back(')');
+            break;
+        }
+        
+        m[rem] = res.size();
+        rem *= 10;
+        res.push_back('0' + rem/d);
+    }
+    return res;
+    
+    /*if (numerator == 0)       //traditional brute force does not works.
+        return "0";
+    if (denominator == 0)
+        return "";
+    long double res = numerator/(long double)denominator;
+    cout  << res << "\n";
+    
+    string resStr = to_string(res);
+    cout << stold(resStr) << "\n";
+    int found = resStr.find('.');
+    
+    auto it = resStr.begin()+found+1;
+    for (; it != resStr.end()-1; ++it)
+    {
+        if (*it != *(it+1))
+            break;
+    }
+    cout << it - resStr.begin()+found+1 << "\n";
+    cout << resStr.end()-1 - resStr.begin()+found+1 << "\n";
+    cout << resStr.length()-1 << "\n";
+    if (it == resStr.end()-1)
+    {
+        resStr.insert(resStr.begin()+found+1, '(');
+        resStr.insert(resStr.begin()+found+3, ')');
+        resStr.erase(found+4);
+    }
+    return resStr;*/
+}
