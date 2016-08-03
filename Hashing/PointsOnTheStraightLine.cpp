24360e527bf0d63e129e284e550d0e64e36b27d7
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-04 01:11:21 +0530

    Create PointsOnTheStraightLine.cpp
    Creating the file.

---
+++ Hashing/PointsOnTheStraightLine.cpp
@@ -0,0 +1,14 @@
+/*
+Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+
+Sample Input :
+
+(1, 1)
+(2, 2)
+Sample Output :
+
+2
+You will be give 2 arrays X and Y. Each point is represented by (X[i], Y[i])
+
+https://www.interviewbit.com/problems/points-on-the-straight-line/
+*/
