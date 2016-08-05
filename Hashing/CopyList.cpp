9ef28f6304c227ed94cbf63fb17a3aab1db796e3
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-06 00:17:58 +0530

    Create CopyList.cpp
    Creating the file.

---
+++ Hashing/CopyList.cpp
@@ -0,0 +1,19 @@
+/*
+A linked list is given such that each node contains an additional random pointer which could point to any node in the list or NULL.
+
+Return a deep copy of the list.
+
+Example
+
+Given list
+
+   1 -> 2 -> 3
+with random pointers going from
+
+  1 -> 3
+  2 -> 1
+  3 -> 1
+You should return a deep copy of the list. The returned answer should not contain the same node as the original list, but a copy of them. The pointers in the returned list should not link to any node in the original input list.
+
+https://www.interviewbit.com/problems/copy-list/
+*/
