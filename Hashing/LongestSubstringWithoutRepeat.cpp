3c96eaadf1cd01754ecdfbdc1f28e943c1c7bb9f
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-07 00:52:26 +0530

    Update LongestSubstringWithoutRepeat.cpp
    Added the code.

--- Hashing/LongestSubstringWithoutRepeat.cpp
+++ Hashing/LongestSubstringWithoutRepeat.cpp
@@ -11,3 +11,33 @@ For "bbbbb" the longest substring is "b", with the length of 1.
 https://www.interviewbit.com/problems/longest-substring-without-repeat/
 */
 
+int Solution::lengthOfLongestSubstring(string A) {
+    if (A.empty())
+        return 0;
+    
+    unordered_map<char, int> ls;
+    int count = 0;
+    int ans = 0;
+    
+    auto q = 0;
+    auto size = A.size();
+    
+    while (q<size)
+    {
+        if (ls.find(A[q]) == ls.end())
+        {
+            ls[A[q]] = q;
+            ++count; ++q;
+        }
+        else
+        {
+            q = ls[A[q]] + 1;
+            ls.clear();
+            ans = max(count, ans);
+            count = 0;
+        }
+    }
+    ans = max(count, ans);
+    
+    return ans;
+}
