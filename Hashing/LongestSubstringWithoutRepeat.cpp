d5370a5f11c415c12154cbfdd6ff706e32ad8fc8
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-07 00:51:57 +0530

    Create LongestSubstringWithoutRepeat.cpp
    Creating the file.

---
+++ Hashing/LongestSubstringWithoutRepeat.cpp
@@ -0,0 +1,13 @@
+/*
+Given a string, 
+find the length of the longest substring without repeating characters.
+
+Example:
+
+The longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3.
+
+For "bbbbb" the longest substring is "b", with the length of 1.
+
+https://www.interviewbit.com/problems/longest-substring-without-repeat/
+*/
+
