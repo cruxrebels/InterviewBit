784ac5c09a78795f088d94e1e94c5d2251b5ded0
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-08 00:06:23 +0530

    Create WindowString.cpp
    Creating the file.

---
+++ Hashing/WindowString.cpp
@@ -0,0 +1,17 @@
+/*
+Given a string S and a string T, find the minimum window in S which will contain all the characters in T in linear time complexity.
+Note that when the count of a character C in T is N, then the count of C in minimum window in S should be atleast N.
+
+Example :
+
+S = "ADOBECODEBANC"
+T = "ABC"
+Minimum window is "BANC"
+
+ Note:
+If there is no such window in S that covers all characters in T, return the empty string ''.
+If there are multiple such windows, return the first occurring minimum window ( with minimum start index ).
+
+https://www.interviewbit.com/problems/window-string/
+*/
+
