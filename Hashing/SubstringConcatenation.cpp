295ddac34a53815f5fe47af70a96c240fa44492d
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-09 00:55:32 +0530

    Update SubstringConcatenation.cpp
    Added the problem statement.

--- Hashing/SubstringConcatenation.cpp
+++ Hashing/SubstringConcatenation.cpp
@@ -1,1 +1,14 @@
+/*
+You are given a string, S, and a list of words, L, that are all of the same length.
 
+Find all starting indices of substring(s) in S that is a concatenation of each word in L exactly once and without any intervening characters.
+
+Example :
+
+S: "barfoothefoobarman"
+L: ["foo", "bar"]
+You should return the indices: [0,9].
+(order does not matter).
+
+https://www.interviewbit.com/problems/substring-concatenation/
+*/
