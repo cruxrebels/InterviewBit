10437d5d2688dff2d16a96a438992bb2b5fa32c7
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-10 23:49:03 +0530

    Create ValidBinarySearchTree.cpp
    Creating the file.

---
+++ Trees/ValidBinarySearchTree.cpp
@@ -0,0 +1,29 @@
+/*
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node’s key.
+The right subtree of a node contains only nodes with keys greater than the node’s key.
+Both the left and right subtrees must also be binary search trees.
+Example :
+
+Input : 
+   1
+  /  \
+ 2    3
+
+Output : 0 or False
+
+
+Input : 
+  2
+ / \
+1   3
+
+Output : 1 or True 
+Return 0 / 1 ( 0 for false, 1 for true ) for this problem
+
+https://www.interviewbit.com/problems/valid-binary-search-tree/
+*/
+
