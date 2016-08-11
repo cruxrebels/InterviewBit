09f5757a9786688f897063492e03c6d7c4ed6aa4
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-11 23:48:30 +0530

    Update NextGreaterNumberBST.cpp
    Added the problem statement.

--- Trees/NextGreaterNumberBST.cpp
+++ Trees/NextGreaterNumberBST.cpp
@@ -1,1 +1,24 @@
+/*
+Given a BST node, return the node which has value just greater than the given node.
+
+Example:
+
+Given the tree
+
+               100
+              /   \
+            98    102
+           /  \
+         96    99
+          \
+           97
+Given 97, you should return the node corresponding to 98 as thats the value just greater than 97 in the tree.
+If there are no successor in the tree ( the value is the largest in the tree, return NULL).
+
+Using recursion is not allowed.
+
+Assume that the value is always present in the tree.
+
+https://www.interviewbit.com/problems/next-greater-number-bst/
+*/
 
