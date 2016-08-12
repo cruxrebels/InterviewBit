f46841a66be7ea7f1a0a4e885799bf8381aa31c6
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-12 23:33:34 +0530

    Update InorderTraversal.cpp
    Added the code.

---
+++ Trees/InorderTraversal.cpp
@@ -0,0 +1,40 @@
+/*
+Given a binary tree, return the inorder traversal of its nodes’ values.
+
+Example :
+Given binary tree
+
+   1
+    \
+     2
+    /
+   3
+return [1,3,2].
+
+Using recursion is not allowed.
+
+https://www.interviewbit.com/problems/inorder-traversal/
+*/
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ void Inorder(TreeNode* root, vector<int>& res)
+ {
+    if (!root)
+        return;
+    Inorder(root->left, res);
+    res.emplace_back(root->val);
+    Inorder(root->right, res);
+ }
+vector<int> Solution::inorderTraversal(TreeNode* A) {
+    vector<int> res;
+    Inorder(A, res);
+    return res;
+}
