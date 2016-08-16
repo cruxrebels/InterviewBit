1e8258df07ae7466d96ae9685ee52b31a68a0413
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-16 23:57:59 +0530

    Update PostorderTraversal.cpp
    Added the code.

--- Trees/PostorderTraversal.cpp
+++ Trees/PostorderTraversal.cpp
@@ -1,1 +1,41 @@
+/*
+Given a binary tree, return the postorder traversal of its nodes’ values.
 
+Example :
+
+Given binary tree
+
+   1
+    \
+     2
+    /
+   3
+return [3,2,1].
+
+Using recursion is not allowed.
+
+https://www.interviewbit.com/problems/postorder-traversal/
+*/
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+void PostOrder(TreeNode* root, vector<int>& res)
+{
+    if (!root)
+        return;
+    PostOrder(root->left, res);
+    PostOrder(root->right, res);
+    res.emplace_back(root->val);
+}
+vector<int> Solution::postorderTraversal(TreeNode* A) {
+    vector<int> res;
+    PostOrder(A, res);
+    return res;
+}
