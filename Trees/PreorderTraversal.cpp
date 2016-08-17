ec1be3e0f1564334f07e5ed983e13f881f0c77da
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-17 22:25:36 +0530

    Update PreorderTraversal.cpp
    Added the code.

--- Trees/PreorderTraversal.cpp
+++ Trees/PreorderTraversal.cpp
@@ -16,3 +16,25 @@ Using recursion is not allowed.
 https://www.interviewbit.com/problems/preorder-traversal/
 */
 
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+void PreOrder(TreeNode* root, vector<int>& res)
+{
+    if (!root)
+        return;
+    res.emplace_back(root->val);
+    PreOrder(root->left, res);
+    PreOrder(root->right, res);
+}
+vector<int> Solution::preorderTraversal(TreeNode* A) {
+    vector<int> res;
+    PreOrder(A, res);
+    return res;
+}
