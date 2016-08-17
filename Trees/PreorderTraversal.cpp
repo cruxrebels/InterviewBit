1a06bc7d99e7932302d82ff30e338a624bdc1ec5
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-17 22:25:10 +0530

    Create PreorderTraversal.cpp
    Creating the file.

---
+++ Trees/PreorderTraversal.cpp
@@ -0,0 +1,18 @@
+/*
+Given a binary tree, return the preorder traversal of its nodes’ values.
+
+Example :
+Given binary tree
+
+   1
+    \
+     2
+    /
+   3
+return [1,2,3].
+
+Using recursion is not allowed.
+
+https://www.interviewbit.com/problems/preorder-traversal/
+*/
+
