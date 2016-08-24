cbe5c802e49ed266a150af777f31ac1c4a002c4d
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-25 01:33:21 +0530

    Create SymmetricBinaryTree.cpp
    Creating the file.

---
+++ Trees/SymmetricBinaryTree.cpp
@@ -0,0 +1,23 @@
+/*
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+Example :
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+The above binary tree is symmetric. 
+But the following is not:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+Return 0 / 1 ( 0 for false, 1 for true ) for this problem
+
+https://www.interviewbit.com/problems/symmetric-binary-tree/
+*/
+
