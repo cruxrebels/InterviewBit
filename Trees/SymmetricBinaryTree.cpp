269758b60d7fd3ead33b0c0093546ffdd1e5e891
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-25 01:34:07 +0530

    Update SymmetricBinaryTree.cpp
    Added the code.

--- Trees/SymmetricBinaryTree.cpp
+++ Trees/SymmetricBinaryTree.cpp
@@ -21,3 +21,25 @@ Return 0 / 1 ( 0 for false, 1 for true ) for this problem
 https://www.interviewbit.com/problems/symmetric-binary-tree/
 */
 
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int isSymmetricHelper(TreeNode* root1, TreeNode* root2)
+{
+    if (!root1 || !root2)
+        return root1 == root2;
+    if (root1->val != root2->val)
+        return 0;
+        
+    return isSymmetricHelper(root1->left, root2->right) && 
+            isSymmetricHelper(root1->right, root2->left);
+}
+int Solution::isSymmetric(TreeNode* A) {
+    return A == NULL || isSymmetricHelper(A->left, A->right);
+}
