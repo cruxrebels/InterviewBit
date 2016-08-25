4e7f0011f6bd5566ff6f2c24a8d9842fbfc091c0
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-26 01:21:03 +0530

    Update IdenticalBinaryTrees.cpp
    Added the code.

--- Trees/IdenticalBinaryTrees.cpp
+++ Trees/IdenticalBinaryTrees.cpp
@@ -19,3 +19,19 @@ Output :
 https://www.interviewbit.com/problems/identical-binary-trees/
 */
 
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int Solution::isSameTree(TreeNode* A, TreeNode* B) {
+    if (!A || !B)
+        return A == B;
+    if (A->val != B->val)
+        return 0;
+    return isSameTree(A->left, B->left) && isSameTree(A->right, B->right);
+}
