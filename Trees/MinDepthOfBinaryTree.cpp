9c5ff540c69408ff99bc122d0a16930a8007aea3
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-30 22:50:59 +0530

    Update MinDepthOfBinaryTree.cpp
    Added the code.

--- Trees/MinDepthOfBinaryTree.cpp
+++ Trees/MinDepthOfBinaryTree.cpp
@@ -14,3 +14,27 @@ min depth = 2.
 https://www.interviewbit.com/problems/min-depth-of-binary-tree/
 */
 
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int depth(TreeNode* A)
+{
+    if (!A)
+        return INT_MAX;
+    if (!A->left && !A->right)
+        return 1;
+    return 1 + min(depth(A->left), depth(A->right));
+}
+int Solution::minDepth(TreeNode* A) {
+    if (!A)
+        return 0;
+    else if (!A->left && !A->right)
+        return 1;
+    return depth(A);
+}
