ede310f49852f6c1e449546397adcb84989ed1f3
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-30 22:09:55 +0530

    Update MaxDepthOfBinaryTree.cpp
    Added the code.

--- Trees/MaxDepthOfBinaryTree.cpp
+++ Trees/MaxDepthOfBinaryTree.cpp
@@ -14,3 +14,17 @@ max depth = 2.
 https://www.interviewbit.com/problems/max-depth-of-binary-tree/
 */
 
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int Solution::maxDepth(TreeNode* A) {
+    if (!A)
+        return 0;
+    return 1 + max(maxDepth(A->left), maxDepth(A->right));
+}
