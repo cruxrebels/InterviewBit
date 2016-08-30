238d036f74aa19c2cf4dded49a913ab51924726d
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-30 22:50:32 +0530

    Create MinDepthOfBinaryTree.cpp
    Creating the file.

---
+++ Trees/MinDepthOfBinaryTree.cpp
@@ -0,0 +1,16 @@
+/*
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+ NOTE : The path has to end on a leaf node. 
+Example :
+
+         1
+        /
+       2
+min depth = 2.
+
+https://www.interviewbit.com/problems/min-depth-of-binary-tree/
+*/
+
