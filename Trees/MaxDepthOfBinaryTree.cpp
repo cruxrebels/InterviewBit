904d7933e92697412abd870139be914506eef935
Abhishek Agrawal <cruxrebels@gmail.com>
2016-08-30 22:09:28 +0530

    Create MaxDepthOfBinaryTree.cpp
    Creating the file.

---
+++ Trees/MaxDepthOfBinaryTree.cpp
@@ -0,0 +1,16 @@
+/*
+Given a binary tree, find its maximum depth.
+
+The maximum depth of a binary tree is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+ NOTE : The path has to end on a leaf node. 
+Example :
+
+         1
+        /
+       2
+max depth = 2.
+
+https://www.interviewbit.com/problems/max-depth-of-binary-tree/
+*/
+
