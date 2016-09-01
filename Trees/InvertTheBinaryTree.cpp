b0ee7caa2e891c09b14859ab4196bc09e2e0dc35
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-01 23:57:34 +0530

    Update InvertTheBinaryTree.cpp
    Added the problem statement.

--- Trees/InvertTheBinaryTree.cpp
+++ Trees/InvertTheBinaryTree.cpp
@@ -1,1 +1,23 @@
+/*
+Given a binary tree, invert the binary tree and return it. 
+Look at the example for more details.
+
+Example : 
+Given binary tree
+
+     1
+   /   \
+  2     3
+ / \   / \
+4   5 6   7
+invert and return
+
+     1
+   /   \
+  3     2
+ / \   / \
+7   6 5   4
+
+https://www.interviewbit.com/problems/invert-the-binary-tree/
+*/
 
