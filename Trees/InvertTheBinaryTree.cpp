f87834964c02d76dff385552a4ab899b15c09536
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-02 01:19:16 +0530

    Update InvertTheBinaryTree.cpp
    Added the code.

--- Trees/InvertTheBinaryTree.cpp
+++ Trees/InvertTheBinaryTree.cpp
@@ -21,3 +21,26 @@ invert and return
 https://www.interviewbit.com/problems/invert-the-binary-tree/
 */
 
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+void invert(TreeNode* root)
+{
+    if (!root || (!root->left && !root->right))
+        return;
+    invert(root->left);
+    invert(root->right);
+    TreeNode* tempLNode = root->left;
+    root->left = root->right;
+    root->right = tempLNode;
+}
+TreeNode* Solution::invertTree(TreeNode* root) {
+    invert(root);
+    return root;
+}
