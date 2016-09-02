1d48d35903a28e07f7a0f416e9f94f84772863c4
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-03 01:42:06 +0530

    Update FlattenBinaryTreeToLinkedList.cpp
    Added the problem statement.

--- Trees/FlattenBinaryTreeToLinkedList.cpp
+++ Trees/FlattenBinaryTreeToLinkedList.cpp
@@ -1,1 +1,30 @@
+/*
+Given a binary tree, flatten it to a linked list in-place.
+
+Example :
+Given
+
+
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+The flattened tree should look like:
+
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+Note that the left child of all nodes should be NULL.
+
+https://www.interviewbit.com/problems/flatten-binary-tree-to-linked-list/
+*/
 
