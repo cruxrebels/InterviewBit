a19d07fcb227bbea5ee530cc82e31c320f12aa3d
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-04 02:36:22 +0530

    Update SortedArrayToBalancedBST.cpp
    Added the code.

--- Trees/SortedArrayToBalancedBST.cpp
+++ Trees/SortedArrayToBalancedBST.cpp
@@ -15,3 +15,30 @@ A height balanced BST  :
 https://www.interviewbit.com/problems/sorted-array-to-balanced-bst/
 */
 
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+TreeNode* createBST(const vector<int>& A, int i, int j)
+{
+    if (i>j)
+        return NULL;
+    auto mid = (i+j) >> 1;
+    TreeNode* root = new TreeNode(A[mid]);
+    if (i==j)
+        return root;
+    root->left = createBST(A, i, mid-1);
+    root->right = createBST(A, mid+1, j);
+    return root;
+}
+TreeNode* Solution::sortedArrayToBST(const vector<int> &A) {
+    if (A.empty())
+        return NULL;
+    return createBST(A, 0, A.size()-1);
+}
