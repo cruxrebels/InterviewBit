fa7da4e855cff38950021e470419cf530799d57e
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-04 02:35:22 +0530

    Create SortedArrayToBalancedBST.cpp
    Creating the file.

---
+++ Trees/SortedArrayToBalancedBST.cpp
@@ -0,0 +1,17 @@
+/*
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+ Balanced tree : a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1. 
+Example :
+
+
+Given A : [1, 2, 3]
+A height balanced BST  : 
+
+      2
+    /   \
+   1     3
+
+https://www.interviewbit.com/problems/sorted-array-to-balanced-bst/
+*/
+
