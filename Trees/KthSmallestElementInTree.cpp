38611c561eb379550adeaee5387ccf3b1b915404
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-05 16:48:16 +0530

    Create KthSmallestElementInTree.cpp
    Creating the file.

---
+++ Trees/KthSmallestElementInTree.cpp
@@ -0,0 +1,20 @@
+/*
+Given a binary search tree, write a function to find the kth smallest element in the tree.
+
+Example :
+
+Input : 
+  2
+ / \
+1   3
+
+and k = 2
+
+Return : 2
+
+As 2 is the second smallest element in the tree.
+ NOTE : You may assume 1 <= k <= Total number of nodes in BST 
+ 
+https://www.interviewbit.com/problems/kth-smallest-element-in-tree/
+*/
+
