5ab6d59e913d000373ee29c6bad0a4f2a9900162
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-05 16:49:01 +0530

    Update KthSmallestElementInTree.cpp
    Added the code.

--- Trees/KthSmallestElementInTree.cpp
+++ Trees/KthSmallestElementInTree.cpp
@@ -18,3 +18,31 @@ As 2 is the second smallest element in the tree.
 https://www.interviewbit.com/problems/kth-smallest-element-in-tree/
 */
 
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+void inorder(TreeNode* root, int& smallest, int k, int& it)
+{
+    if (!root)
+        return;
+    inorder(root->left, smallest, k, it);
+    ++it;
+    if (it == k)
+    {
+        smallest = root->val;
+        return;
+    }
+    inorder(root->right, smallest, k, it);
+}
+int Solution::kthsmallest(TreeNode* root, int k) {
+    int smallest = -1;
+    int it = 0;
+    inorder(root, smallest, k, it);
+    return smallest;
+}
