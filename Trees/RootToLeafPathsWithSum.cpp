156a24f26a5d9185dd0742173d176a607abd6c71
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-08 23:58:41 +0530

    Create RootToLeafPathsWithSum.cpp
    Creating the file.

---
+++ Trees/RootToLeafPathsWithSum.cpp
@@ -0,0 +1,23 @@
+/*
+Given a binary tree and a sum, find all root-to-leaf paths where each path’s sum equals the given sum.
+
+For example:
+Given the below binary tree and sum = 22,
+
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \    / \
+        7    2  5   1
+return
+
+[
+   [5,4,11,2],
+   [5,8,4,5]
+]
+
+https://www.interviewbit.com/problems/root-to-leaf-paths-with-sum/
+*/
+
