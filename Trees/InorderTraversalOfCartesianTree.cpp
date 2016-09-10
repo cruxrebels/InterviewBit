7788c1ea0a2e731f0eef5eb810540d9f6ba27e87
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-10 22:51:48 +0530

    Create InorderTraversalOfCartesianTree.cpp
    Creating the file.

---
+++ Trees/InorderTraversalOfCartesianTree.cpp
@@ -0,0 +1,19 @@
+/*
+Given an inorder traversal of a cartesian tree, construct the tree.
+
+ Cartesian tree : is a heap ordered binary tree, where the root is greater than all the elements in the subtree. 
+ Note: You may assume that duplicates do not exist in the tree. 
+Example :
+
+Input : [1 2 3]
+
+Return :   
+          3
+         /
+        2
+       /
+      1
+      
+https://www.interviewbit.com/problems/inorder-traversal-of-cartesian-tree/
+*/
+
