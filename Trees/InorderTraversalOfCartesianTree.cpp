0d0df9299c7e369444ec7b0052881b0e145fa8d0
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-10 22:52:18 +0530

    Update InorderTraversalOfCartesianTree.cpp
    Added the code.

--- Trees/InorderTraversalOfCartesianTree.cpp
+++ Trees/InorderTraversalOfCartesianTree.cpp
@@ -17,3 +17,37 @@ Return :
 https://www.interviewbit.com/problems/inorder-traversal-of-cartesian-tree/
 */
 
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int maxVal(int i, int j, vector<int>& A)
+{
+    int maxi = i;
+    for (auto p = i+1; p<=j; ++p)
+        if (A[maxi]<A[p])
+            maxi = p;
+    return maxi;
+}
+
+TreeNode* makeTree(int i, int j, vector<int>& A)
+{
+    if (i>j)
+        return NULL;
+    auto r = maxVal(i, j, A);
+    TreeNode* node = new TreeNode(A[r]);
+    node->left = makeTree(i, r-1, A);
+    node->right = makeTree(r+1, j, A);
+    return node;
+}
+
+TreeNode* Solution::buildTree(vector<int> &A) {
+    if (A.empty())
+        return NULL;
+    return makeTree(0, A.size()-1, A);
+}
