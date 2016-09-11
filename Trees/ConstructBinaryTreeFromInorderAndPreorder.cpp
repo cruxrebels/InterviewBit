edf96f6b491fc2baf23efdc08c48d28b77006b2d
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-11 11:58:28 +0530

    Create ConstructBinaryTreeFromInorderAndPreorder.cpp
    Creating the file.

---
+++ Trees/ConstructBinaryTreeFromInorderAndPreorder.cpp
@@ -0,0 +1,17 @@
+/*
+Given preorder and inorder traversal of a tree, construct the binary tree.
+
+ Note: You may assume that duplicates do not exist in the tree. 
+Example :
+
+Input :
+        Preorder : [1, 2, 3]
+        Inorder  : [2, 1, 3]
+
+Return :
+            1
+           / \
+          2   3
+
+https://www.interviewbit.com/problems/construct-binary-tree-from-inorder-and-preorder/
+*/
