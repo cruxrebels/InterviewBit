26c538ed60a1b97eb3dd52b80430050b33c9de5e
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-11 11:58:54 +0530

    Update ConstructBinaryTreeFromInorderAndPreorder.cpp
    Added the code.

--- Trees/ConstructBinaryTreeFromInorderAndPreorder.cpp
+++ Trees/ConstructBinaryTreeFromInorderAndPreorder.cpp
@@ -15,3 +15,37 @@ Return :
 
 https://www.interviewbit.com/problems/construct-binary-tree-from-inorder-and-preorder/
 */
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int findIndex(vector<int> &inorder, int i, int j, int val)
+{
+    for (auto b = i; b<=j; ++b)
+        if (inorder[b]==val)
+            return b;
+}
+TreeNode* makeTree(vector<int> &preorder, vector<int> &inorder, int i, int j, int& p)
+{
+    if (i>j)
+        return NULL;
+    TreeNode* node = new TreeNode(preorder[p++]);
+    if (i==j)
+        return node;
+    int in = findIndex(inorder, i, j, node->val);
+    node->left = makeTree(preorder, inorder, i, in-1, p);
+    node->right = makeTree(preorder, inorder, in+1, j, p);
+    return node;
+}
+TreeNode* Solution::buildTree(vector<int> &preorder, vector<int> &inorder) {
+    if (preorder.empty() || inorder.empty())
+        return NULL;
+    int p = 0;
+    return makeTree(preorder, inorder, 0, inorder.size()-1, p);
+}
