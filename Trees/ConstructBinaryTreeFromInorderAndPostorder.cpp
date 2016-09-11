e6df288094c54ad0d78466ca53b638c2079feb9d
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-11 16:46:53 +0530

    Create ConstructBinaryTreeFromInorderAndPostorder.cpp
    Creating the file.

---
+++ Trees/ConstructBinaryTreeFromInorderAndPostorder.cpp
@@ -0,0 +1,18 @@
+/*
+Given inorder and postorder traversal of a tree, construct the binary tree.
+
+ Note: You may assume that duplicates do not exist in the tree. 
+Example :
+
+Input : 
+        Inorder : [2, 1, 3]
+        Postorder : [2, 3, 1]
+
+Return : 
+            1
+           / \
+          2   3
+
+https://www.interviewbit.com/problems/construct-binary-tree-from-inorder-and-postorder/
+*/
+
