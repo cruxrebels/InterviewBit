7d283038a643ef09cced7ae7a805e1b62e290add
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-12 23:42:29 +0530

    Create ShortestUniquePrefix.cpp
    Creating the file.

---
+++ Trees/ShortestUniquePrefix.cpp
@@ -0,0 +1,17 @@
+/*
+Find shortest unique prefix to represent each word in the list.
+
+Example:
+
+Input: [zebra, dog, duck, dove]
+Output: {z, dog, du, dov}
+where we can see that
+zebra = z
+dog = dog
+duck = du
+dove = dov
+ NOTE : Assume that no word is prefix of another. In other words, the representation is always possible.
+ 
+https://www.interviewbit.com/problems/shortest-unique-prefix/
+*/
+
