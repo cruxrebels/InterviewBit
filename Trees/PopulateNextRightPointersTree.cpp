ba3b9261af1e8da46f6a946085351dbfa3eab69d
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-15 23:44:42 +0530

    Update PopulateNextRightPointersTree.cpp
    Added the problem statement.

--- Trees/PopulateNextRightPointersTree.cpp
+++ Trees/PopulateNextRightPointersTree.cpp
@@ -1,1 +1,36 @@
+/*
+Given a binary tree
+
+    struct TreeLinkNode {
+      TreeLinkNode *left;
+      TreeLinkNode *right;
+      TreeLinkNode *next;
+    }
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+ Note:
+You may only use constant extra space.
+Example :
+
+Given the following binary tree,
+
+         1
+       /  \
+      2    3
+     / \  / \
+    4  5  6  7
+After calling your function, the tree should look like:
+
+         1 -> NULL
+       /  \
+      2 -> 3 -> NULL
+     / \  / \
+    4->5->6->7 -> NULL
+ Note 1: that using recursion has memory overhead and does not qualify for constant space.
+ Note 2: The tree need not be a perfect binary tree.
+ 
+https://www.interviewbit.com/problems/populate-next-right-pointers-tree/
+*/
 
