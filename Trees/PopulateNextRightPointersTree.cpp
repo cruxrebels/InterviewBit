cc5d24c3998ff288c244b9715935012f35144896
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-16 00:28:24 +0530

    Update PopulateNextRightPointersTree.cpp
    Added the code.

--- Trees/PopulateNextRightPointersTree.cpp
+++ Trees/PopulateNextRightPointersTree.cpp
@@ -34,3 +34,80 @@ After calling your function, the tree should look like:
 https://www.interviewbit.com/problems/populate-next-right-pointers-tree/
 */
 
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+TreeLinkNode* nextRight(TreeLinkNode* current)
+{
+    TreeLinkNode* currNext = current->next;
+    
+    while (currNext)
+    {
+        if (currNext->left)
+            return currNext->left;
+        else if (currNext->right)
+            return currNext->right;
+        currNext = currNext->next;
+    }
+    return NULL;
+}
+void Solution::connect(TreeLinkNode* A) {
+    if (!A)
+        return;
+    A->next = NULL;
+    while (A)
+    {
+        TreeLinkNode* B = A;
+        while (B)
+        {
+            if (B->left)
+            {
+                if (B->right)
+                    B->left->next = B->right;
+                else
+                    B->left->next = nextRight(B);
+            }
+            if (B->right)
+                B->right->next = nextRight(B);
+                
+            B = B->next;
+        }
+        if (A->left)
+            A = A->left;
+        else if (A->right)
+            A = A->right;
+        else
+            A = nextRight(A);
+    }
+    
+    // Queue approach, less time complexity
+    /*queue<TreeLinkNode*> Q;
+    if (A)
+        Q.push(A);
+    A->next = NULL;
+    
+    while (!Q.empty())
+    {
+        TreeLinkNode* node = Q.front();
+        if (node->left)
+        {
+            Q.push(node->left);
+            if (node->right)
+                node->left->next = node->right;
+            else
+                node->left->next = nextRight(node);
+        }
+        if (node->right)
+        {
+            Q.push(node->right);
+            node->right->next = nextRight(node);
+        }
+        
+        Q.pop();
+    }*/
+}
