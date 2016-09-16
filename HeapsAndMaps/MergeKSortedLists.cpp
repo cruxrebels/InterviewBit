64594b36573bb4b72ac59aa5a4ba8b0c6942d8c0
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-17 00:25:44 +0530

    Create MergeKSortedLists.cpp
    Creating the file.

---
+++ HeapsAndMaps/MergeKSortedLists.cpp
@@ -0,0 +1,15 @@
+/*
+Merge k sorted linked lists and return it as one sorted list.
+
+Example :
+
+1 -> 10 -> 20
+4 -> 11 -> 13
+3 -> 8 -> 9
+will result in
+
+1 -> 3 -> 4 -> 8 -> 9 -> 10 -> 11 -> 13 -> 20
+
+https://www.interviewbit.com/problems/merge-k-sorted-lists/
+*/
+
