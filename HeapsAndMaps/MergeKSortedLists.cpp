760ff9dcc6a56c7d3c378775c8fc0d7774f88d68
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-17 00:26:10 +0530

    Update MergeKSortedLists.cpp
    Added the code.

--- HeapsAndMaps/MergeKSortedLists.cpp
+++ HeapsAndMaps/MergeKSortedLists.cpp
@@ -13,3 +13,43 @@ will result in
 https://www.interviewbit.com/problems/merge-k-sorted-lists/
 */
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+struct Compare
+{
+    bool operator()(ListNode* const& a, ListNode* const& b)
+    {
+        return a->val > b->val;
+    }
+};
+
+ListNode* Solution::mergeKLists(vector<ListNode*> &A) {
+    if (A.empty())
+        return NULL;
+    ListNode* dummy = new ListNode(0);
+    ListNode* tail = dummy;
+    priority_queue<ListNode*, vector<ListNode*>, Compare> PQ;
+    
+    for (auto i = 0; i<A.size(); ++i)
+    {
+        if (A[i])
+            PQ.emplace(A[i]);
+    }
+    
+    while (!PQ.empty())
+    {
+        tail->next = PQ.top();
+        PQ.pop();
+        tail = tail->next;
+        if (tail->next)
+            PQ.emplace(tail->next);
+    }
+    return dummy->next;
+}
