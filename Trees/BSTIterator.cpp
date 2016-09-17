78bc99d7db4ab15367b568923047f12f6e539324
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-18 02:33:56 +0530

    Create BST Iterator
    Creating the file.

---
+++ Trees/BSTIterator.cpp
@@ -0,0 +1,11 @@
+/*
+Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
+
+The first call to next() will return the smallest number in BST. Calling next() again will return the next smallest number in the BST, and so on.
+
+ Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+Try to optimize the additional space complexity apart from the amortized time complexity. 
+
+https://www.interviewbit.com/problems/bst-iterator/
+*/
+
