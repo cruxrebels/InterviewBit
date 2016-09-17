105ad79ce63fa96984eb686c0e67954656b979f3
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-18 02:34:18 +0530

    Update BSTIterator.cpp
    Added the code.

--- Trees/BSTIterator.cpp
+++ Trees/BSTIterator.cpp
@@ -9,3 +9,45 @@ Try to optimize the additional space complexity apart from the amortized time co
 https://www.interviewbit.com/problems/bst-iterator/
 */
 
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+stack<TreeNode*> path;
+void pushall(TreeNode* root)
+{
+    while (root)
+    {
+        path.push(root);
+        root = root->left;
+    }
+}
+
+BSTIterator::BSTIterator(TreeNode *root) {
+    pushall(root);
+}
+
+/** @return whether we have a next smallest number */
+bool BSTIterator::hasNext() {
+    return !path.empty();
+}
+
+/** @return the next smallest number */
+int BSTIterator::next() {
+    TreeNode* tmp = path.top();
+    path.pop();
+    pushall(tmp->right);
+    return tmp->val;
+}
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = BSTIterator(root);
+ * while (i.hasNext()) cout << i.next();
+ */
