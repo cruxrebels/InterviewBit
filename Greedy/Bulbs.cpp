618a0e13bbcb56caccff9cf9fcd24ff6b162cb2a
Abhishek Agrawal <cruxrebels@gmail.com>
2016-10-02 21:31:54 +0530

    Update Bulbs.cpp
    Added the code.

--- Greedy/Bulbs.cpp
+++ Greedy/Bulbs.cpp
@@ -17,4 +17,22 @@ Explanation :
  https://www.interviewbit.com/problems/bulbs/
  */
  
- 
+ int Solution::bulbs(vector<int> &A) {
+    int count = 0;
+    bool toggle = 0;
+    for (auto i = 0; i<A.size(); ++i)
+    {
+        if (toggle)
+            A[i] = 1 - A[i];
+        if (A[i] == 0)
+        {
+            A[i] = 1;
+            if (!toggle)
+                toggle = 1;
+            else
+                toggle = 0;
+            ++count;
+        }
+    }
+    return count;
+}
