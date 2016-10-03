2c638212b076ada6e1155bad3c9a6d73db90333c
Abhishek Agrawal <cruxrebels@gmail.com>
2016-10-03 23:07:11 +0530

    Update DistributeCandy.cpp
    Added the code.

--- Greedy/DistributeCandy.cpp
+++ Greedy/DistributeCandy.cpp
@@ -18,3 +18,22 @@ So rating 2 candidate gets 2 candies. In total, 2+1 = 3 candies need to be given
 https://www.interviewbit.com/problems/distribute-candy/
 */
 
+int Solution::candy(vector<int> &ratings) {
+    int candies = 0;
+    int size = ratings.size();
+    vector<int> lefttoright(size, 1);
+    vector<int> righttoleft(size, 1);
+    
+    for (auto l = 1; l<size; ++l)
+        if (ratings[l] > ratings[l-1])
+            lefttoright[l] = lefttoright[l-1] + 1;
+    
+    for (auto r = size-2; r>=0; --r)
+        if (ratings[r] > ratings[r+1])
+            righttoleft[r] = righttoleft[r+1] + 1;
+            
+    for (auto i = 0; i<size; ++i)
+        candies += max(lefttoright[i], righttoleft[i]);
+        
+    return candies;
+}
