e4c5a13b2ae9491df99fbc8a2000516e1e62885b
Abhishek Agrawal <cruxrebels@gmail.com>
2016-10-03 22:48:32 +0530

    Create DistributeCandy.cpp
    Creating the file.

---
+++ Greedy/DistributeCandy.cpp
@@ -0,0 +1,20 @@
+/*
+There are N children standing in a line. Each child is assigned a rating value.
+
+ You are giving candies to these children subjected to the following requirements:
+Each child must have at least one candy.
+Children with a higher rating get more candies than their neighbors.
+What is the minimum candies you must give?
+
+Sample Input :
+
+Ratings : [1 2]
+Sample Output :
+
+3
+The candidate with 1 rating gets 1 candy and candidate with rating cannot get 1 candy as 1 is its neighbor. 
+So rating 2 candidate gets 2 candies. In total, 2+1 = 3 candies need to be given out.
+
+https://www.interviewbit.com/problems/distribute-candy/
+*/
+
