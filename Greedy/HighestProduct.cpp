e62ac5b7e5a883bd326f1ea12391fa16fcf5d3a7
Abhishek Agrawal <cruxrebels@gmail.com>
2016-10-09 00:35:33 +0530

    Update HighestProduct.cpp
    Added the problem statement.

--- Greedy/HighestProduct.cpp
+++ Greedy/HighestProduct.cpp
@@ -1,1 +1,15 @@
+/*
+Given an array of integers, return the highest product possible by multiplying 3 numbers from the array
 
+Input:
+
+array of integers e.g {1, 2, 3}
+ NOTE: Solution will fit in a 32-bit signed integer 
+Example:
+
+[0, -1, 3, 100, 70, 50]
+
+=> 70*50*100 = 350000
+
+https://www.interviewbit.com/problems/highest-product/
+*/
