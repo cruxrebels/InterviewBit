f5d3b7cd2f937ff094cf27eeb7fc323724e239e4
Abhishek Agrawal <cruxrebels@gmail.com>
2016-10-11 00:20:08 +0530

    Update Seats.cpp
    Added the code.

--- Greedy/Seats.cpp
+++ Greedy/Seats.cpp
@@ -38,3 +38,80 @@ In that case the total number of jumps will be
     
 https://www.interviewbit.com/problems/seats/
 */
+
+#define MOD 10000003
+int Solution::seats(string A) {
+    auto n = A.length();
+    
+    // Median approach
+    vector<int> pos;
+    for (auto i = 0; i<n; ++i)
+        if (A[i] == 'x')
+            pos.emplace_back(i);
+    
+    auto psize = pos.size();
+    
+    if (psize == 0)
+        return 0;
+    int mid = psize/2;
+    int median = psize & 1 ? pos[mid] : (pos[mid-1] + pos[mid])/2;
+    
+    int hops = 0; 
+    int empty = A[median] == 'x' ? median - 1 : median;
+    
+    for (auto s = median-1; s>=0; --s)
+        if (A[s] == 'x')
+        {
+            hops += empty - s;
+            hops %= MOD;
+            --empty;
+        }
+            
+    empty = median + 1;
+    for (auto e = median+1; e<n; ++e)
+        if (A[e] == 'x')
+        {
+            hops += e - empty;
+            hops %= MOD;
+            ++empty;
+        }
+    
+    return hops % MOD;
+    
+    // My attempt of pushing it near max x count - failed for corner cases
+    /*int maxCount = INT_MIN, startIndex = 0, endIndex = 0;
+    int hops = 0;
+    for (auto i = 0; i < n; ++i)
+    {
+        int count = 0, index = i;
+        while (A[i] == 'x' && i < n)
+        {
+            ++count;
+            ++i;
+        }
+        if (count > maxCount)
+        {
+            maxCount = count;
+            startIndex = index;
+            endIndex = i - 1;
+        }
+    }
+    cout << maxCount << " " << startIndex << endIndex << endl;
+    auto f1 = startIndex, f2 = endIndex;
+    for (auto j = 0; j < n; ++j)
+    {
+        if (A[j] == 'x' && !(j >= f1 && j <= f2))
+        {
+            int d1 = abs(startIndex - 1 - j);
+            int d2 = abs(endIndex + 1 - j);
+            if (d1<=d2)
+                if (startIndex > 0)
+                    --startIndex;
+            else
+                if (endIndex < n-1)
+                    ++endIndex;
+            hops += min (d1, d2);
+        }
+    }
+    return hops % 10000003;*/
+}
