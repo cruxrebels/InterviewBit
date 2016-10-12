0a43a13f09295a8bf5fef15159f59ba1dc85ad2f
Abhishek Agrawal <cruxrebels@gmail.com>
2016-10-12 23:14:15 +0530

    Update CoinSumInfinite.cpp
    Added the problem statement.

--- DynamicProgramming/CoinSumInfinite.cpp
+++ DynamicProgramming/CoinSumInfinite.cpp
@@ -1,1 +1,22 @@
+/*
+You are given a set of coins S. In how many ways can you make sum N assuming you have infinite amount of each coin in the set.
 
+Note : Coins in set S will be unique. Expected space complexity of this problem is O(N).
+
+Example :
+
+Input : 
+	S = [1, 2, 3] 
+	N = 4
+
+Return : 4
+
+Explanation : The 4 possible ways are
+{1, 1, 1, 1}
+{1, 1, 2}
+{2, 2}
+{1, 3}	
+Note that the answer can overflow. So, give us the answer % 1000007
+
+https://www.interviewbit.com/problems/coin-sum-infinite/
+*/
