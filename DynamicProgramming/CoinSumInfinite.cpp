52e05450a92fc89be17b6cd5aea433a4cf3565a0
Abhishek Agrawal <cruxrebels@gmail.com>
2016-10-12 23:29:11 +0530

    Update CoinSumInfinite.cpp
    Added the code.

--- DynamicProgramming/CoinSumInfinite.cpp
+++ DynamicProgramming/CoinSumInfinite.cpp
@@ -20,3 +20,18 @@ Note that the answer can overflow. So, give us the answer % 1000007
 
 https://www.interviewbit.com/problems/coin-sum-infinite/
 */
+
+#define mod 1000007
+int Solution::coinchange2(vector<int> &A, int B) {
+    int table[B+1],n=A.size();
+    memset(table,0,sizeof(table));
+    table[0]=1;
+    int i,j;
+    for(i=0;i<n;i++){
+        for(j=A[i];j<=B;j++){
+            table[j]+=table[j-A[i]];
+            table[j]%=mod;
+        }
+    }
+    return table[B];
+}
