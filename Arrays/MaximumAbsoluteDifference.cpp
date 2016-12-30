eaf32f0df52a33dbd0af2d007d99ca6fe907b887
Abhishek Agrawal <cruxrebels@gmail.com>
2016-12-30 23:56:37 +0530

    Create MaximumAbsoluteDifference.cpp
    Creating the file.

---
+++ Arrays/MaximumAbsoluteDifference.cpp
@@ -0,0 +1,18 @@
+/*
+You are given an array of N integers, A1, A2 ,…, AN. Return maximum value of f(i, j) for all 1 ≤ i, j ≤ N.
+f(i, j) is defined as |A[i] - A[j]| + |i - j|, where |x| denotes absolute value of x.
+
+For example,
+
+A=[1, 3, -1]
+
+f(1, 1) = f(2, 2) = f(3, 3) = 0
+f(1, 2) = f(2, 1) = |1 - 3| + |1 - 2| = 3
+f(1, 3) = f(3, 1) = |1 - (-1)| + |1 - 3| = 4
+f(2, 3) = f(3, 2) = |3 - (-1)| + |2 - 3| = 5
+
+So, we return 5.
+
+https://www.interviewbit.com/problems/maximum-absolute-difference/
+*/
+
