9344f4615a65b6ea58a4c2a266b59e8e1c2125db
Abhishek Agrawal <cruxrebels@gmail.com>
2017-02-03 22:25:29 +0530

    Create KthSmallestElementInTheArray.cpp
    Added the problem statement.

---
+++ CheckpointProblems/Level3/KthSmallestElementInTheArray.cpp
@@ -0,0 +1,20 @@
+/*
+Find the kth smallest element in an unsorted array of non-negative integers.
+
+Definition of kth smallest element
+
+ kth smallest element is the minimum possible n such that there are at least k elements in the array <= n.
+In other words, if the array A was sorted, then A[k - 1] ( k is 1 based, while the arrays are 0 based ) 
+NOTE
+You are not allowed to modify the array ( The array is read only ). 
+Try to do it using constant extra space.
+
+Example:
+
+A : [2 1 4 3 2]
+k : 3
+
+answer : 2
+
+https://www.interviewbit.com/problems/kth-smallest-element-in-the-array/
+*/
