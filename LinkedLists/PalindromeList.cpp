8147bac614723026765dff93f8b1320bb479ade1
Abhishek Agrawal <cruxrebels@gmail.com>
2017-02-04 16:11:00 +0530

    Update PalindromeList.cpp
    Added the code.

--- LinkedLists/PalindromeList.cpp
+++ LinkedLists/PalindromeList.cpp
@@ -11,3 +11,36 @@ List 1-->2-->3 is not a palindrome.
 
 https://www.interviewbit.com/problems/palindrome-list/#
 */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+int Solution::lPalin(ListNode* A) {
+    if (!A || !A->next)
+        return 1;
+    stack<int> s;
+    ListNode* slow = A, *fast = A;
+    while (fast && fast->next)
+    {
+        s.push(slow->val);
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    
+    if (fast)
+        slow = slow->next;
+    
+    while (s.size() && slow)
+    {
+        if (s.top() != slow->val)
+            return 0;
+        s.pop();
+        slow = slow->next;
+    }
+    return 1;
+}
