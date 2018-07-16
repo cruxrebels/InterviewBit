6dc0ddd08cfe900f8a77b22b0389fdf90781f329
Abhishek Agrawal <cruxrebels@gmail.com>
2018-07-17 02:10:46 +0530

    Update SpiralOrderMatrixI.cpp
    Added the problem statement in comments.

--- Arrays/SpiralOrderMatrixI.cpp
+++ Arrays/SpiralOrderMatrixI.cpp
@@ -1,3 +1,22 @@
+/*
+Given a matrix of m * n elements (m rows, n columns), return all elements of the matrix in spiral order.
+
+Example:
+
+Given the following matrix:
+
+[
+    [ 1, 2, 3 ],
+    [ 4, 5, 6 ],
+    [ 7, 8, 9 ]
+]
+You should return
+
+[1, 2, 3, 6, 9, 8, 7, 4, 5]
+
+https://www.interviewbit.com/problems/spiral-order-matrix-i/
+*/
+
 vector<int> Solution::spiralOrder(const vector<vector<int> > &A) {
     int rows = A.size();
     if (rows == 0)
