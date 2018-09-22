eb154d138a12374e6e4f7144acc074a96653f445
Abhishek Agrawal <cruxrebels@gmail.com>
2018-09-23 01:28:43 +0530

    Update BinaryRepresentation.cpp
    Added int + '0' method

--- Math/BinaryRepresentation.cpp
+++ Math/BinaryRepresentation.cpp
@@ -10,6 +10,7 @@ binary form = 110
 https://www.interviewbit.com/problems/binary-representation/
 */
 
+// Solution 1
 string Solution::findDigitsInBinary(int A) {
     int r;
     string result;
@@ -21,6 +22,21 @@ string Solution::findDigitsInBinary(int A) {
     {
         r = A%2;
         result += to_string(r); //Or result.push_back((char)('0' + r)); will also work
+        A /= 2;
+    }
+    reverse(result.begin(), result.end());
+    return result;
+}
+
+// Solution 2
+string Solution::findDigitsInBinary(int A) {
+    if (A == 0)
+        return "0";
+    
+    string result = "";
+    
+    while (A != 0) {
+        result += A%2 + '0';
         A /= 2;
     }
     reverse(result.begin(), result.end());
