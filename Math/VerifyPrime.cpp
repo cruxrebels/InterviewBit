7016ffb7cb0d212ee65d6d5ad4cbdcecc6a9ccb8
Abhishek Agrawal <cruxrebels@gmail.com>
2018-09-23 01:29:52 +0530

    Update VerifyPrime.cpp
    Added problem statement.

--- Math/VerifyPrime.cpp
+++ Math/VerifyPrime.cpp
@@ -1,3 +1,16 @@
+/*
+Given a number N, verify if N is prime or not.
+
+Return 1 if N is prime, else return 0.
+
+Example :
+
+Input : 7
+Output : True
+
+https://www.interviewbit.com/problems/verify-prime/
+*/
+
 int Solution::isPrime(int A) {
     if (A<2)
         return 0;
