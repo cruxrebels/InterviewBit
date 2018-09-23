c0d0b3b59d4348d1b9d92cff125afeb070eee106
Abhishek Agrawal <cruxrebels@gmail.com>
2018-09-23 14:04:07 +0530

    Update PrimeSum.cpp
    Added problem statement and extra solutions.

--- Math/PrimeSum.cpp
+++ Math/PrimeSum.cpp
@@ -1,3 +1,51 @@
+/*
+Given an even number ( greater than 2 ), return two prime numbers whose sum will be equal to given number.
+
+NOTE A solution will always exist. read Goldbach’s conjecture
+
+Example:
+
+
+Input : 4
+Output: 2 + 2 = 4
+
+If there are more than one solutions possible, return the lexicographically smaller solution.
+
+If [a, b] is one solution with a <= b,
+and [c,d] is another solution with c <= d, then
+
+[a, b] < [c, d] 
+
+If a < c OR a==c AND b < d. 
+
+https://www.interviewbit.com/problems/prime-sum/
+*/
+
+// Solution 1 - Simple and without extra space
+bool isPrime(int A) {
+    for (auto i = 2; i*i <= A; ++i)
+        if (A%i == 0)
+            return false;
+    return true;
+}
+
+vector<int> Solution::primesum(int A) {
+    vector<int> res;
+    if (A%2 != 0)
+        return res;
+    
+    for (auto j = 2; j <= A/2; ++j) {
+        auto diff = A-j;
+        if (isPrime(j) && isPrime(diff)) {
+            res.emplace_back(j);
+            res.emplace_back(diff);
+            break;
+        }
+    }
+    return res;
+}
+
+// Solution 2 - Uses extra space with arr showing whether prime or not
 bool isPrime(int n)
 {
     for(int i=2; i*i<=n; ++i)
@@ -18,7 +66,6 @@ vector<int> Solution::primesum(int A) {
     {
         if(arr[i]==0)
         {
-            //prime.emplace_back(i);
             if(isPrime(A-i))
             {
                 result.emplace_back(i);
@@ -29,5 +76,48 @@ vector<int> Solution::primesum(int A) {
                 arr[i*j] = 1;
         }   
     }
+    return result;
+}
+
+// Solution 3 - Uses extra space and complex write up
+void findPrimes(vector<int>* result, int A)
+{
+    vector<bool> temp(A+1, true);
+    temp[0] = temp[1] = false;
+    for (auto i = 2; i*i<=A; ++i)
+    {
+        for (auto j = i*2; j<=A; j += i)
+            temp[j] = false;
+    }
+    for (auto k = 2; k<A+1; ++k)
+    {
+        if (temp[k] && temp[A-k])
+        {
+            (*result).emplace_back(k);
+            (*result).emplace_back(A-k);
+            break;
+        }
+    }
+}
+
+vector<int> Solution::primesum(int A) {
+    if (A % 2 != 0)
+        return {};
+    vector<int> result;
+    //vector<int> primes;
+    findPrimes(&result, A);
+    /*for(auto i = 0; i<primes.size(); ++i) 
+    {
+        const auto& it = find_if(primes.cbegin(), primes.cend(), [&primes, &i, &A] (const int& prime)
+        {
+            return A - primes[i] == prime;
+        });
+        if (it != primes.cend())
+        {
+            result.emplace_back(primes[i]);
+            result.emplace_back(*it);
+            break;
+        }
+    }*/
     return result;
 }
