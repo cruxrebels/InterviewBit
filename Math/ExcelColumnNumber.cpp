3e76c3320d8024df232247dfd83d28212d72a84b
Abhishek Agrawal <cruxrebels@gmail.com>
2018-09-30 10:44:22 +0530

    Update ExcelColumnNumber.cpp
    Added the problem statement.

--- Math/ExcelColumnNumber.cpp
+++ Math/ExcelColumnNumber.cpp
@@ -1,3 +1,25 @@
+/*
+Given a column title as appears in an Excel sheet, return its corresponding column number.
+
+Example:
+
+    A -> 1
+    
+    B -> 2
+    
+    C -> 3
+    
+    ...
+    
+    Z -> 26
+    
+    AA -> 27
+    
+    AB -> 28 
+    
+https://www.interviewbit.com/problems/excel-column-number/
+*/
+
 int Solution::titleToNumber(string A) {
      auto n = A.length();
      int value = 0;
