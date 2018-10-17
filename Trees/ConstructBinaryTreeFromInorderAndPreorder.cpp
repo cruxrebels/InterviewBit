5a22a8677a3f3df4ce95b83ea42f1dcab4034882
atul94 <atulhyd2013@outlook.com>
2018-10-17 14:55:10 +0530

    Reduce time complexity to O(n)
    Also made the program non-recursive

--- Trees/ConstructBinaryTreeFromInorderAndPreorder.cpp
+++ Trees/ConstructBinaryTreeFromInorderAndPreorder.cpp
@@ -25,27 +25,31 @@ https://www.interviewbit.com/problems/construct-binary-tree-from-inorder-and-pre
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-int findIndex(vector<int> &inorder, int i, int j, int val)
-{
-    for (auto b = i; b<=j; ++b)
-        if (inorder[b]==val)
-            return b;
-}
-TreeNode* makeTree(vector<int> &preorder, vector<int> &inorder, int i, int j, int& p)
-{
-    if (i>j)
-        return NULL;
-    TreeNode* node = new TreeNode(preorder[p++]);
-    if (i==j)
-        return node;
-    int in = findIndex(inorder, i, j, node->val);
-    node->left = makeTree(preorder, inorder, i, in-1, p);
-    node->right = makeTree(preorder, inorder, in+1, j, p);
-    return node;
-}
 TreeNode* Solution::buildTree(vector<int> &preorder, vector<int> &inorder) {
-    if (preorder.empty() || inorder.empty())
-        return NULL;
-    int p = 0;
-    return makeTree(preorder, inorder, 0, inorder.size()-1, p);
+    vector<TreeNode *> st;
+    int ptrInOrder = 0;
+    int ptrPreOrder = 0;
+    TreeNode * root;
+    TreeNode * temp;
+    root = new TreeNode(preorder[ptrPreOrder++]);
+    st.push_back(root);
+    while(ptrPreOrder < preorder.size()){
+        if(st.back()->val == inorder[ptrInOrder]){
+            temp = st.back();
+            st.pop_back();
+            ptrInOrder++;
+            if(!st.empty() && st.back()->val == inorder[ptrInOrder]){
+                continue;
+            }else{
+                temp->right = new TreeNode(preorder[ptrPreOrder++]);
+                st.push_back(temp->right);
+            }
+            
+        }else{
+            temp = new TreeNode(preorder[ptrPreOrder++]);
+            st.back()->left = temp;
+            st.push_back(temp);
+        }
+    }
+    return root;
 }
