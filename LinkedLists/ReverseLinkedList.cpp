3085b3a8b6eef52749dd74a7a1ce41f8d31d3818
Abhishek Agrawal <cruxrebels@gmail.com>
2018-10-27 11:52:37 +0530

    Update ReverseLinkedList.cpp
    Added recursive solution.

--- LinkedLists/ReverseLinkedList.cpp
+++ LinkedLists/ReverseLinkedList.cpp
@@ -17,7 +17,28 @@ https://www.interviewbit.com/problems/reverse-linked-list/
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// Recursive approach
+/*ListNode* RecursiveReverse(ListNode* prev, ListNode* curr, ListNode* head)
+{
+    if (!curr->next)
+    {
+        curr->next = prev;
+        head = curr;
+        return head;
+    }
+    ListNode* next = curr->next;
+    curr->next = prev;
+    return RecursiveReverse(curr, next, head);
+}*/
+
 ListNode* Solution::reverseList(ListNode* A) {
+    // Recursive approach
+    /*if (!A)
+        return A;
+    return RecursiveReverse(NULL, A, A);*/
+    
+    // Iterative approach
     ListNode* prev = NULL;
     ListNode* current = A;
     ListNode* next = A;
