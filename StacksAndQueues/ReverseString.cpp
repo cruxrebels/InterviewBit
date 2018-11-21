c66275430a3cf3c027372ff3559c29d59a42b0d3
Abhishek Agrawal <cruxrebels@gmail.com>
2018-11-22 00:14:20 +0530

    Update ReverseString.cpp
    Added a more concise solution.

--- StacksAndQueues/ReverseString.cpp
+++ StacksAndQueues/ReverseString.cpp
@@ -11,36 +11,13 @@ https://www.interviewbit.com/problems/reverse-string/
 
 string Solution::reverseString(string A) {
     stack<char> s;
-    auto size = A.length();
-    
-    for(auto i=0; i<size; ++i)
-        s.push(A[i]);
+    for (const auto& a : A) {
+        s.push(a);
+    }
     A.clear();
-    for(auto j=0; j<size; ++j)
-    {
-        if(!s.empty())
-        {
-            A += s.top();
-            s.pop();
-        }
+    while (!s.empty()) {
+        A += s.top();
+        s.pop();
     }
     return A;
 }
-
-/*string Solution::reverseString(string A) {
-    stack<char> s;
-    auto size = A.length();
-    
-    for(auto i=0; i<size; ++i)
-        s.push(A[i]);
-    A.clear();  //Not necessarily required since the new string length is going to be the same. See above approach.
-    for(auto j=0; j<size; ++j)
-    {
-        if(!s.empty())
-        {
-            A += s.top();   //One way to do it
-            s.pop();
-        }
-    }
-    return A;
-}*/
