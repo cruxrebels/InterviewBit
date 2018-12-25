bc67baef3d1a4d05d1fcfdefa1f02ae4e9632cb1
Abhishek Agrawal <cruxrebels@gmail.com>
2018-12-26 00:06:04 +0530

    Update FindDuplicateInArray.cpp
    Added Floyd Warshal's technique

--- Arrays/FindDuplicateInArray.cpp
+++ Arrays/FindDuplicateInArray.cpp
@@ -24,3 +24,21 @@ int Solution::repeatedNumber(const vector<int> &A) {
     }
     return -1;
 }
+
+/*
+// Floyd Warshal's Cycle detecting algorithm treating the Array as a LL
+int Solution::repeatedNumber(const vector<int> &A) {
+    int slow = A[0], fast = A[A[0]];
+    while (slow != fast) {
+        slow = A[slow];
+        fast = A[A[fast]];
+    }
+    
+    slow = 0;
+    while (slow != fast) {
+        slow = A[slow];
+        fast = A[fast];
+    }
+    return slow;
+}
+*/
