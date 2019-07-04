d3eb60a916a4632d22c73f7d30373676dce5d4a8
Nishtha Paul <33231250+nishthapaul@users.noreply.github.com>
2019-07-04 19:47:08 +0530

    Update LargestRectangleInHistogram.cpp

--- StacksAndQueues/LargestRectangleInHistogram.cpp
+++ StacksAndQueues/LargestRectangleInHistogram.cpp
@@ -1,35 +1,26 @@
 int Solution::largestRectangleArea(vector<int> &hist) {
     stack<int> s; 
-  
-    int max_area = 0; // Initalize max area 
-    int tp;  // To store top of stack 
-    int area_with_top; // To store area with top bar 
+    int max_area = 0;
+    int tp;
+    int area_with_top; 
     int i = 0; 
-    while (i < hist.size()) 
-    { 
+    while (i < hist.size()) { 
         if (s.empty() || hist[s.top()] <= hist[i]) 
-            s.push(i++); 
-
-        else
-        { 
-            tp = s.top();  // store the top index 
-            s.pop();  // pop the top 
-
+            s.push(i++);
+        else { 
+            tp = s.top();
+            s.pop();
             area_with_top = hist[tp] * (s.empty() ? i : i - s.top() - 1); 
-  
             if (max_area < area_with_top) 
                 max_area = area_with_top; 
         } 
-    } 
-  
+    }
     while (s.empty() == false) { 
         tp = s.top(); 
         s.pop(); 
         area_with_top = hist[tp] * (s.empty() ? i : i - s.top() - 1); 
-  
         if (max_area < area_with_top) 
             max_area = area_with_top; 
-    } 
-  
+    }
     return max_area; 
 } 
