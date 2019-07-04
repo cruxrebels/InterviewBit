fc0ac2af664e5dc6a56a0d067742a5cb1489bd26
Abhishek Agrawal <cruxrebels@gmail.com>
2019-07-04 20:55:00 +0530

    Update LargestRectangleInHistogram.cpp
    Added the problem statement with problem URL.

--- StacksAndQueues/LargestRectangleInHistogram.cpp
+++ StacksAndQueues/LargestRectangleInHistogram.cpp
@@ -1,3 +1,25 @@
+/*
+Given n non-negative integers representing the histogram’s bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+
+Largest Rectangle in Histogram: Example 1
+
+http://i.imgur.com/1OutEEI.png
+
+Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
+
+http://i.imgur.com/F2bePvG.png
+
+Largest Rectangle in Histogram: Example 2
+
+The largest rectangle is shown in the shaded area, which has area = 10 unit.
+
+For example,
+Given height = [2,1,5,6,2,3],
+return 10.
+
+https://www.interviewbit.com/problems/largest-rectangle-in-histogram/
+*/
+
 int Solution::largestRectangleArea(vector<int> &hist) {
     stack<int> s; 
     int max_area = 0;
