fb1cb1fd6d9b9e3ba1cdf71e8a5a66c76f887113
Devesh511 <36008010+Devesh511@users.noreply.github.com>
2019-10-23 02:05:32 +0530

    Update RainWaterTrapped.cpp

--- StacksAndQueues/RainWaterTrapped.cpp
+++ StacksAndQueues/RainWaterTrapped.cpp
@@ -16,19 +16,17 @@ https://www.interviewbit.com/problems/rain-water-trapped/
 */
 
 int Solution::trap(const vector<int> &arr) {
-    
-    int n=arr.size();
+    int n = arr.size();
     int left[n];  
     int right[n]; 
     int water = 0;  
     left[0] = arr[0]; 
-    for (int i = 1; i < n; i++) 
+    for (auto i = 1; i < n; ++i) 
        left[i] = max(left[i-1], arr[i]);   
     right[n-1] = arr[n-1]; 
-    for (int i = n-2; i >= 0; i--) 
+    for (auto i = n-2; i >= 0; --i) 
        right[i] = max(right[i+1], arr[i]); 
-    for (int i = 0; i < n; i++) 
+    for (auto i = 0; i < n; --i) 
        water += min(left[i],right[i]) - arr[i]; 
-  
     return water;
 }
