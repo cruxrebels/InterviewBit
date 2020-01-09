6df9a5920c48408cf941f21a2ecf316c97448fb8
BharatNischal <33544814+BharatNischal@users.noreply.github.com>
2020-01-09 14:16:26 +0530

    Solution to Median of Array

--- BinarySearch/MedianOfArray.cpp
+++ BinarySearch/MedianOfArray.cpp
@@ -1,3 +1,26 @@
+/*
+There are two sorted arrays A and B of size m and n respectively.
+
+Find the median of the two sorted arrays ( The median of the array formed by merging both the arrays ).
+
+The overall run time complexity should be O(log (m+n)).
+
+Sample Input
+
+A : [1 4 5]
+B : [2 3]
+
+Sample Output
+
+3
+
+ NOTE: IF the number of elements in the merged array is even, then the median is the average of n / 2 th and n/2 + 1th element.
+For example, if the array is [1 2 3 4], the median is (2 + 3) / 2.0 = 2.5
+
+https://www.interviewbit.com/problems/median-of-array/
+
+*/
+
 double Solution::findMedianSortedArrays(const vector<int> &A, const vector<int> &B) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
